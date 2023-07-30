6f5be37997ff55389f7ebad73482965f9e640767
elythh <elyth@tuta.io>
2023-07-30 15:23:37 +0200

    :wrench: fix changeTheme

--- home/shared/bin/misc/changeTheme.nix
+++ home/shared/bin/misc/changeTheme.nix
@@ -3,8 +3,8 @@ _:
   #!/usr/bin/env sh
   THEME=$1
   sed -i "5s/.*/  colors = import ..\/shared\/cols\/$THEME.nix { };/g" /etc/nixos/home/gwen/home.nix
-  cd /etc/nixos && home-manager switch --flake ".#$USER"
+  cd /etc/nixos && home-manager switch --flake ".#$USER@thinkpad"
   echo $THEME > /tmp/themeName
-  kill -USR1 $(pidof st)
+  kill -USR1 $(pidof kitty)
   awesome-client 'awesome.restart()'
 ''
