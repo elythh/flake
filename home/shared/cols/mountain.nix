3bff0755d800e62f5139d5cc941086fc5090c303
elythh <elyth@tuta.io>
2023-08-03 18:59:53 +0200

    :zap: new colorschene : mountain

---
+++ home/shared/cols/mountain.nix
@@ -0,0 +1,38 @@
+{}:
+rec {
+  wallpaper = "road.jpeg";
+  foreground = "f0f0f0";
+  background = "0f0f0f";
+  darker = "1d2125";
+  bg2 = "24292e";
+  mbg = "262626";
+
+  cursorColor = "dcdee6";
+  contrast = "1c1f26";
+  cursorline = "1cf216";
+  comment = "56585b";
+
+  color8 = "262626";
+  color0 = "4c4c4c";
+
+  color1 = "ac8a8c";
+  color9 = "c49ea0";
+  color2 = "8aac8b";
+  color10 = "9ec49f";
+  color3 = "aca98a";
+  color11 = "c4c19e";
+
+  color4 = "8f8aac";
+  color12 = "a39ec4";
+
+  color5 = "ac8aac";
+  color13 = "c49ec4";
+
+  color6 = "8aacab";
+  color14 = "9ec3c4";
+
+  color15 = "e7e7e7";
+  color7 = "f0f0f0";
+  name = "mountain";
+  neofetchpic = "awesome.png";
+}
