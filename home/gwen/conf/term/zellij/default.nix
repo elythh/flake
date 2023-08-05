ef1cedc44aff8ea1ca4b4d7b4d8d63ad9206e6ef
elythh <elyth@tuta.io>
2023-08-05 11:03:44 +0200

    :pencil2: zellij conf

--- home/gwen/conf/term/zellij/default.nix
+++ home/gwen/conf/term/zellij/default.nix
@@ -4,16 +4,17 @@ with colors; {
   programs.zellij = {
     enable = true;
     enableZshIntegration = false;
-    settings.themes.default = import ./colors.nix {
-      inherit colors;
-    };
-    settings.default_layout = "compact";
-    settings.simplified_ui = true;
-    settings.scrollback_editor = "/home/gwen/.nix-profile/bin/nvim";
-    settings.pane_frames = false;
-    settings.on_force_close = "quit";
-    settings.keybinds = {
-      unbind = [ "Ctrl b" "Ctrl h" ];
+    settings = {
+      themes.default = import ./colors.nix { inherit colors; };
+      default_layout = "compact";
+      mouse_mode = false;
+      simplified_ui = true;
+      scrollback_editor = "/home/gwen/.nix-profile/bin/nvim";
+      pane_frames = false;
+      on_force_close = "quit";
+      keybinds = {
+          unbind = [ "Ctrl b" "Ctrl h" ];
+        };
     };
   };
 }
