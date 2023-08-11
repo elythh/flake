2bb14d37918f0a78e67cdbd400187cddf7f20c99
elythh <elyth@tuta.io>
2023-08-11 18:52:23 +0200

    :pencil2: zellij refact

--- home/gwen/conf/term/zellij/default.nix
+++ home/gwen/conf/term/zellij/default.nix
@@ -7,14 +7,14 @@ with colors; {
     settings = {
       themes.default = import ./colors.nix { inherit colors; };
       default_layout = "compact";
-      mouse_mode = false;
+      mouse_mode = true;
       simplified_ui = true;
       scrollback_editor = "/home/gwen/.nix-profile/bin/nvim";
       pane_frames = false;
-      on_force_close = "quit";
+      on_force_close = "detach";
       keybinds = {
-          unbind = [ "Ctrl b" "Ctrl h" ];
-        };
+        unbind = [ "Ctrl b" "Ctrl h" ];
+      };
     };
   };
 }
