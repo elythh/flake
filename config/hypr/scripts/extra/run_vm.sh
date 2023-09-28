dc683b43db2e7517ee057813b717c38e3a265219
elythh <elyth@tuta.io>
2023-09-28 23:24:39 +0200

    WIP :zap: hyprland config

---
+++ config/hypr/scripts/extra/run_vm.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+virsh --connect qemu:///system start windows11
+
+looking-glass-client -m KEY_RIGHTCTRL
+
+# destroy windows session when window is closed
+virsh --connect qemu:///system destroy windows11
