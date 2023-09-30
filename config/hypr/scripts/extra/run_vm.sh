96e30fbf326e6613915200a30756558870f54dbf
elythh <elyth@tuta.io>
2023-09-30 18:25:25 +0200

    WIP WIP WIP :construction: HYPRLAND CONFIG THIS IS NOT WORKING YET

--- config/hypr/scripts/extra/run_vm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-virsh --connect qemu:///system start windows11
-
-looking-glass-client -m KEY_RIGHTCTRL
-
-# destroy windows session when window is closed
-virsh --connect qemu:///system destroy windows11
--- config/hypr/themes/minimal/waybar/scss/watch.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sass --watch --no-source-map ~/.config/hypr/themes/minimal/waybar/scss/style.scss ~/.config/hypr/themes/minimal/waybar/style.css
