2885dc2fde15d0de788854c7c7e3faa3a1257bd6
elythh <gwenchlan@tuta.io>
2023-10-05 13:11:57 +0200

    :zap: direnv

---
+++ home/gwen/conf/utils/direnv/default.nix
@@ -0,0 +1,13 @@
+{
+  # ...other config, other config...
+
+  programs = {
+    direnv = {
+      enable = true;
+      enableBashIntegration = true; # see note on other shells below
+      nix-direnv.enable = true;
+    };
+
+    bash.enable = true; # see note on other shells below
+  };
+}
