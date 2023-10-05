a688abb8f19c060dcf20f2945bda4a774e587fb1
elythh <gwenchlan@tuta.io>
2023-10-05 11:33:21 +0200

    :zap: back at it again with the white vans

--- shell.nix
+++ shell.nix
@@ -4,6 +4,12 @@
 { pkgs ? (import ./nixpkgs.nix) { } }: {
   default = pkgs.mkShell {
     # Enable experimental features without having to specify the argument
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [
+      stdenv.cc.cc
+      openssl
+      # ...
+    ];
+    NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [ nix home-manager git ];
   };
