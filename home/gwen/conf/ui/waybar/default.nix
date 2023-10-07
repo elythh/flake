c52636addd672e811670b4309ee1e65aad50a945
elythh <gwenchlan@tuta.io>
2023-10-07 20:57:46 +0200

    :wrench: chore

--- home/gwen/conf/ui/waybar/default.nix
+++ home/gwen/conf/ui/waybar/default.nix
@@ -4,7 +4,6 @@
   programs.waybar =
     with colors;{
       enable = true;
-      # package = hyprland.packages.${pkgs.system}.waybar-hyprland;
       package = pkgs.waybar;
       systemd = {
         enable = false;
