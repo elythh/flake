cb0b7368e394e24d173436e113cb24ffeaa51d48
elythh <gwenchlan@tuta.io>
2023-10-22 17:29:19 +0200

    :wrench: hyprland - monitors

--- home/gwen/conf/ui/hyprland/default.nix
+++ home/gwen/conf/ui/hyprland/default.nix
@@ -28,9 +28,15 @@
         #########################################################################################
 
         # You have to change this based on your monitor 
+
+        ### WORK SCREEN SETUP
+        # monitor=eDP-1,1920x1080@60,960x1080,1
+        # monitor=DP-6,1920x1080@60,0x0,1
+        # monitor=DP-3,highres,1920x0,1
+        ### HOME SCREEN SETUP
+
         monitor=eDP-1,1920x1080@60,2560x0,1
-        #monitor=DP-6,1920x1080@60,0x0,1
-        monitor=DP-3,highres,0x0,1
+        monitor=DP-3,highres,0,1
         # Status bar :) 
         exec-once=eww open bar
         exec-once=eww open osd
