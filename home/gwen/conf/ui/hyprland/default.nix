65e7617b60a0aaaf87a1ef1670c7e0bcb4afb0b1
elythh <gwenchlan@tuta.io>
2023-10-28 19:58:44 +0200

    :pencil2: new hyprland conf

--- home/gwen/conf/ui/hyprland/default.nix
+++ home/gwen/conf/ui/hyprland/default.nix
@@ -1,277 +1,235 @@
-{ config, lib, pkgs, hyprland, hyprland-plugins, split-monitor-workspaces, colors, ... }:
+{ inputs, pkgs, ... }:
+let
+  hyprland = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  plugins = inputs.hyprland-plugins.packages.${pkgs.system};
 
+  launcher = pkgs.writeShellScriptBin "hypr" ''
+    #!/${pkgs.bash}/bin/bash
+
+    export WLR_NO_HARDWARE_CURSORS=1
+    export _JAVA_AWT_WM_NONREPARENTING=1
+
+    exec ${hyprland}/bin/Hyprland
+  '';
+in
 {
-  systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
-  systemd.user.services.swayidle.Install.WantedBy = lib.mkForce [ "hyprland-session.target" ];
+  home.packages = [ launcher ];
+
+  xdg.desktopEntries."org.gnome.Settings" = {
+    name = "Settings";
+    comment = "Gnome Control Center";
+    icon = "org.gnome.Settings";
+    exec = "env XDG_CURRENT_DESKTOP=gnome ${pkgs.gnome.gnome-control-center}/bin/gnome-control-center";
+    categories = [ "X-Preferences" ];
+    terminal = false;
+  };
+
   wayland.windowManager.hyprland = {
     enable = true;
-    plugins = [
-      split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
-      # hyprland-plugins.packages.${pkgs.system}.hyprbars
-    ];
-    package = hyprland.packages.${pkgs.system}.hyprland;
+    package = hyprland;
     systemd.enable = true;
-    extraConfig = ''
-        ########################################################################################
-         __  __ _       _                 _ 
-        |  \/  (_)_ __ (_)_ __ ___   __ _| |
-        | |\/| | | '_ \| | '_ ` _ \ / _` | |
-        | |  | | | | | | | | | | | | (_| | |
-        |_|  |_|_|_| |_|_|_| |_| |_|\__,_|_|
-                                    
-         _   _                  _                 _    ____             __ _           
-        | | | |_   _ _ __  _ __| | __ _ _ __   __| |  / ___|___  _ __  / _(_) __ _ ___ 
-        | |_| | | | | '_ \| '__| |/ _` | '_ \ / _` | | |   / _ \| '_ \| |_| |/ _` / __|
-        |  _  | |_| | |_) | |  | | (_| | | | | (_| | | |__| (_) | | | |  _| | (_| \__ \
-        |_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_|  \____\___/|_| |_|_| |_|\__, |___/
-               |___/|_|                                                      |___/    
-        #########################################################################################
+    xwayland.enable = true;
+    # plugins = with plugins; [ hyprbars borderspp ];
 
-        # You have to change this based on your monitor 
+    settings = {
+      exec-once = [
+        "ags -b hypr"
+        "hyprctl setcursor Qogir 24"
+        "transmission-gtk"
+      ];
 
-        ### WORK SCREEN SETUP
-        # monitor=eDP-1,1920x1080@60,960x1080,1
-        # monitor=DP-8,1920x1080@60,0x0,1
-        # monitor=DP-3,highres,1920x0,1
-        ### HOME SCREEN SETUP
+      monitor = [
+        "eDP-1, 1920x1080, 0x0, 1"
+        "HDMI-A-1, 2560x1440, 1920x0, 1"
+      ];
 
-        monitor=eDP-1,1920x1080@60,2560x0,1
-        monitor=DP-3,highres,0,1
-        # Status bar :) 
-        exec-once=ags -b hypr
-        exec-once=hyprland-autoname-workspaces
+      general = {
+        layout = "dwindle";
+        resize_on_border = true;
+      };
 
-        #Notification 
-        exec-once=dunst
-        # Wallpaper
-        exec-once=swww init
-        # For screen sharing 
-        exec-once=dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
-        # For keyboard 
-        exec-once=fcitx5 -D
+      misc = {
+        layers_hog_keyboard_focus = false;
+        disable_splash_rendering = true;
+        force_default_wallpaper = 0;
+      };
 
-        # Bluetooth
-        exec-once=blueman-applet # Make sure you have installed blueman
+      input = {
+        kb_layout = "us";
+        kb_options = compose:caps;
+        repeat_rate = 50;
+        repeat_delay = 240;
+        follow_mouse = 1;
+        touchpad = {
+          natural_scroll = "yes";
+          disable_while_typing = true;
+          drag_lock = true;
+        };
+        sensitivity = 0;
+        float_switch_override_focus = 2;
+      };
 
-        # Screen Sharing 
-        exec-once=systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+      binds = {
+        allow_workspace_cycles = true;
+      };
 
-        input {
-          kb_layout = us
-          kb_variant = 
-          kb_options = compose:caps
-          repeat_rate=50
-          repeat_delay=240
+      dwindle = {
+        pseudotile = "yes";
+        preserve_split = "yes";
+        # no_gaps_when_only = "yes";
+      };
 
-          touchpad {
-            disable_while_typing=1
-            natural_scroll=1
-            clickfinger_behavior=1
-            middle_button_emulation=0
-            tap-to-click=1
-          }
-        }
+      gestures = {
+        workspace_swipe = "on";
+        workspace_swipe_direction_lock = false;
+        workspace_swipe_forever = true;
+        workspace_swipe_numbered = true;
+      };
 
-        gestures { 
-          workspace_swipe=true 
-          workspace_swipe_min_speed_to_force=5
-        }
+      windowrule =
+        let
+          f = regex: "float, ^(${regex})$";
+        in
+        [
+          (f "org.gnome.Calculator")
+          (f "org.gnome.Nautilus")
+          (f "pavucontrol")
+          (f "nm-connection-editor")
+          (f "blueberry.py")
+          (f "org.gnome.Settings")
+          (f "org.gnome.design.Palette")
+          (f "Color Picker")
+          (f "xdg-desktop-portal")
+          (f "xdg-desktop-portal-gnome")
+          (f "transmission-gtk")
+          "workspace 7, title:Spotify"
+        ];
 
-        general {
-            layout=dwindle
-            sensitivity=1.0 # for mouse cursor
+      bind =
+        let
+          binding = mod: cmd: key: arg: "${mod}, ${key}, ${cmd}, ${arg}";
+          mvfocus = binding "SUPER" "movefocus";
+          ws = binding "SUPER" "workspace";
+          resizeactive = binding "SUPER CTRL" "resizeactive";
+          mvactive = binding "SUPER ALT" "moveactive";
+          mvtows = binding "SUPER SHIFT" "movetoworkspace";
+          e = "exec, ags -b hypr";
+          arr = [ 1 2 3 4 5 6 7 8 9 ];
+        in
+        [
+          "CTRL SHIFT, R,  ${e} quit; ags -b hypr"
+          "SUPER, R,       ${e} -t applauncher"
+          ", XF86PowerOff, ${e} -t powermenu"
+          "SUPER, Tab,     ${e} -t overview"
+          ", XF86Launch4,  ${e} -r 'recorder.start()'"
+          ",Print,         ${e} -r 'recorder.screenshot()'"
+          "SHIFT,Print,    ${e} -r 'recorder.screenshot(true)'"
+          "SUPER, Return, exec, wezterm"
+          "SUPER SHIFT, Return, exec, wezterm -e ~/.config/hypr/scripts/zellij.sh"
+          "SUPER SHIFT, W, exec, ~/.local/bin/wallpicker"
+          "SUPER SHIFT,D,exec, tessen -d rofi"
+          "SUPER CTRL,L,exec, swaylock"
+          "SUPER, E, exec, wezterm -e lf"
 
-            gaps_in=5
-            gaps_out=15
-            border_size=2
-            col.active_border=0x${colors.color3}ac
-            col.inactive_border=0x${colors.bg2}33
+          "ALT, Tab, focuscurrentorlast"
+          "CTRL ALT, Delete, exit"
+          "ALT, Q, killactive"
+          "SUPER, F, togglefloating"
+          "SUPER, G, fullscreen"
+          "SUPER, O, fakefullscreen"
+          "SUPER, P, togglesplit"
 
-            apply_sens_to_raw=0 # whether to apply the sensitivity to raw input (e.g. used by games where you aim using your mouse)
-        }
+          (mvfocus "k" "u")
+          (mvfocus "j" "d")
+          (mvfocus "l" "r")
+          (mvfocus "h" "l")
+          (ws "left" "e-1")
+          (ws "right" "e+1")
+          (mvtows "left" "e-1")
+          (mvtows "right" "e+1")
+          (resizeactive "k" "0 -20")
+          (resizeactive "j" "0 20")
+          (resizeactive "l" "20 0")
+          (resizeactive "h" "-20 0")
+          (mvactive "k" "0 -20")
+          (mvactive "j" "0 20")
+          (mvactive "l" "20 0")
+          (mvactive "h" "-20 0")
+        ]
+        ++ (map (i: ws (toString i) (toString i)) arr)
+        ++ (map (i: mvtows (toString i) (toString i)) arr);
 
+      bindle = let e = "exec, ags -b hypr -r"; in
+        [
+          ",XF86MonBrightnessUp,   ${e} 'brightness.screen += 0.05; indicator.display()'"
+          ",XF86MonBrightnessDown, ${e} 'brightness.screen -= 0.05; indicator.display()'"
+          ",XF86KbdBrightnessUp,   ${e} 'brightness.kbd++; indicator.kbd()'"
+          ",XF86KbdBrightnessDown, ${e} 'brightness.kbd--; indicator.kbd()'"
+          ",XF86AudioRaiseVolume,  ${e} 'audio.speaker.volume += 0.05; indicator.speaker()'"
+          ",XF86AudioLowerVolume,  ${e} 'audio.speaker.volume -= 0.05; indicator.speaker()'"
+        ];
 
-      decoration {
-        rounding = 16
-        blur {
-          enabled = true
-          size = 15
-          passes = 3
-          new_optimizations = true
-          brightness = 1.0
-          contrast = 1.0
-          noise = 0.02
-        }
-  
-        drop_shadow = true
-        shadow_ignore_window = true
-        shadow_offset = 0 2
-        shadow_range = 20
-        shadow_render_power = 3
-        col.shadow = rgba(00000055)
-      }
+      bindl = let e = "exec, ags -b hypr -r"; in
+        [
+          ",XF86AudioPlay,    ${e} 'mpris()?.playPause()'"
+          ",XF86AudioStop,    ${e} 'mpris()?.stop()'"
+          ",XF86AudioPause,   ${e} 'mpris()?.pause()'"
+          ",XF86AudioPrev,    ${e} 'mpris()?.previous()'"
+          ",XF86AudioNext,    ${e} 'mpris()?.next()'"
+          ",XF86AudioMicMute, ${e} 'audio.microphone.isMuted = !audio.microphone.isMuted'"
+        ];
 
-      blurls=lockscreen
-      $layers = ^(eww-.+|bar|system-menu|anyrun|rofi|gtk-layer-shell)$
-       layerrule = blur, $layers
-       layerrule = ignorealpha 0, $layers
-       layerrule = ignorealpha 0.5, ^(eww-music)$
+      bindm = [
+        "SUPER, mouse:273, resizewindow"
+        "SUPER, mouse:272, movewindow"
+      ];
 
-      animations {
-        enabled = true
-        animation = border, 1, 2, default
-        animation = fade, 1, 4, default
-        animation = windows, 1, 3, default, popin 80%
-        animation = workspaces, 1, 2, default, slide
-      }
+      decoration = {
+        drop_shadow = "yes";
+        shadow_range = 8;
+        shadow_render_power = 2;
+        "col.shadow" = "rgba(00000044)";
 
-        dwindle {
-            pseudotile=1 # enable pseudotiling on dwindle
-            # force_split=2
-            force_split=1
-        }
+        dim_inactive = false;
 
-        master {
-          new_on_top=true
-        }
+        blur = {
+          enabled = true;
+          size = 8;
+          passes = 3;
+          new_optimizations = "on";
+          noise = 0.01;
+          contrast = 0.9;
+          brightness = 0.8;
+        };
+      };
 
-        misc {
-          disable_hyprland_logo=true
-          disable_splash_rendering=true
-          mouse_move_enables_dpms=true
-          vfr = false
-        }
+      animations = {
+        enabled = "yes";
+        bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+        animation = [
+          "windows, 1, 5, myBezier"
+          "windowsOut, 1, 7, default, popin 80%"
+          "border, 1, 10, default"
+          "fade, 1, 7, default"
+          "workspaces, 1, 6, default"
+        ];
+      };
 
-        plugin {
-          split-monitor-workspaces {
-            count = 5
-          }
-        }
+      plugin = {
+        hyprbars = {
+          bar_color = "rgb(2a2a2a)";
+          bar_height = 28;
+          col_text = "rgba(ffffffdd)";
+          bar_text_size = 11;
+          bar_text_font = "Ubuntu Nerd Font";
 
-        ########################################################################################
-
-        \ \        / (_)         | |                   |  __ \     | |          
-          \ \  /\  / / _ _ __   __| | _____      _____  | |__) |   _| | ___  ___ 
-           \ \/  \/ / | | '_ \ / _` |/ _ \ \ /\ / / __| |  _  / | | | |/ _ \/ __|
-            \  /\  /  | | | | | (_| | (_) \ V  V /\__ \ | | \ \ |_| | |  __/\__ \
-             \/  \/   |_|_| |_|\__,_|\___/ \_/\_/ |___/ |_|  \_\__,_|_|\___||___/
-
-        ########################################################################################
-
-
-        # Float Necessary Windows
-        windowrule=float,Rofi
-        windowrule=float,pavucontrol
-        windowrulev2 = float,class:^()$,title:^(Picture in picture)$
-        windowrulev2 = float,class:^(brave)$,title:^(Save File)$
-        windowrulev2 = float,class:^(brave)$,title:^(Open File)$
-        windowrulev2 = float,class:^(LibreWolf)$,title:^(Picture-in-Picture)$
-        windowrulev2 = float,class:^(blueman-manager)$
-        windowrulev2 = float,class:^(org.twosheds.iwgtk)$
-        windowrulev2 = float,class:^(blueberry.py)$
-        windowrulev2 = float,class:^(xdg-desktop-portal-gtk)$
-        windowrulev2 = float,class:^(geeqie)$
-
-        # Increase the opacity 
-        windowrule=opacity 0.92,Spotify
-        windowrule=opacity 0.92,Thunar
-        windowrule=opacity 0.96,discord
-        windowrule=opacity 0.9,VSCodium
-        windowrule=opacity 0.88,obsidian
-
-        ^.*nvim.*$
-        windowrule=tile,librewolf
-        windowrule=tile,spotify
-        windowrule=opacity 1,neovim
-        bindm=SUPER,mouse:272,movewindow
-        bindm=SUPER,mouse:273,resizewindow
-
-        ###########################################
-          ____  _           _ _                 
-         |  _ \(_)         | (_)                
-         | |_) |_ _ __   __| |_ _ __   __ _ ___ 
-         |  _ <| | '_ \ / _` | | '_ \ / _` / __|
-         | |_) | | | | | (_| | | | | | (_| \__ \
-         |____/|_|_| |_|\__,_|_|_| |_|\__, |___/
-                                       __/ |    
-                                      |___/     
-
-        ###########################################
-
-        # example binds
-        bind=SUPER,Q,killactive
-        bind=SUPER,F,fullscreen,1
-        bind=SUPERSHIFT,F,fullscreen,0
-        bind=SUPER,RETURN,exec,kitty
-        bind=SUPER,Z,exec,kitty -e ~/.config/hypr/scripts/zellij.sh
-        bind=SUPER,W,exec, ~/.local/bin/wallpicker
-        bind=SUPERSHIFT,T,exec, ~/.local/bin/themepicker
-        bind=SUPERSHIFT,Q,exit,
-        bind=SUPER,D,exec, rofi -show drun
-        bind=SUPERSHIFT,D,exec, tessen -d rofi
-        bind=SUPER,P,pseudo,
-        bind=SUPERCTRL,L,exec, swaylock
-        bind=SUPER,O,togglegroup
-
-        bind=SUPER,ESCAPE,exec,systemctl suspend
-
-        bind=,XF86AudioMute,exec,~/.config/hypr/scripts/volume mute
-        bind=,XF86AudioLowerVolume,exec,~/.config/hypr/scripts/volume down
-        bind=,XF86AudioRaiseVolume,exec,~/.config/hypr/scripts/volume up
-        bind=,XF86AudioMicMute,exec,pactl set-source-mute @DEFAULT_SOURCE@ toggle
-
-        bindle=,XF86MonBrightnessUp,exec,~/.config/hypr/scripts/brightness up  # increase screen brightness
-        bindle=,XF86MonBrightnessDown,exec,~/.config/hypr/scripts/brightness down # decrease screen brightnes
-        bind=SUPERSHIFT,C,exec,bash ~/.config/hypr/scripts/hyprPicker.sh
-        bind=SUPERSHIFT,E,exec,wlogout
-        bind = SUPER, T, togglefloating,
-        bind=SUPERSHIFT,P,exec,pomotroid --in-process-gpu
-
-        # Screen shot 
-        bind=SUPERSHIFT,S,exec,grim -g "$(slurp)" - | swappy -f -
-        bind=SUPERCTRL,S,exec,~/.config/hypr/scripts/screenshots.sh
-        # Screen recorder
-        bind=SUPER,R,exec,wf-recorder -g "$(slurp)"
-        # Emoji selector 
-        bind=SUPER,E,exec,anyrun
-
-
-        bind=SUPERSHIFT,RETURN,layoutmsg,swapwithmaster
-
-        # bind=SUPER,j,layoutmsg,cyclenext
-        # bind=SUPER,k,layoutmsg,cycleprev
-
-        bind=SUPER,j,movefocus,d
-        bind=SUPER,k,movefocus,u
-
-        bind=SUPER,h,movefocus,l
-        bind=SUPER,l,movefocus,r
-
-        bind=SUPER,left,resizeactive,-40 0
-        bind=SUPER,right,resizeactive,40 0
-
-        bind=SUPER,up,resizeactive,0 -40
-        bind=SUPER,down,resizeactive,0 40
-
-        bind=SUPERSHIFT,h,movewindow,l
-        bind=SUPERSHIFT,l,movewindow,r
-        bind=SUPERSHIFT,k,movewindow,u
-        bind=SUPERSHIFT,j,movewindow,d
-
-        bind = SUPER, 1, split-workspace, 1
-        bind = SUPER, 2, split-workspace, 2
-        bind = SUPER, 3, split-workspace, 3
-        bind = SUPER, 4, split-workspace, 4
-        bind = SUPER, 5, split-workspace, 5
-
-        bind = SUPER SHIFT, 1, split-movetoworkspacesilent, 1
-        bind = SUPER SHIFT, 2, split-movetoworkspacesilent, 2
-        bind = SUPER SHIFT, 3, split-movetoworkspacesilent, 3
-        bind = SUPER SHIFT, 4, split-movetoworkspacesilent, 4
-        bind = SUPER SHIFT, 5, split-movetoworkspacesilent, 5
-
-        bind = SUPER, y, exec, scratchpad
-        bind = SUPERSHIFT, y, exec, scratchpad -g
-    '';
+          buttons = {
+            button_size = 0;
+            "col.maximize" = "rgba(ffffff11)";
+            "col.close" = "rgba(ff111133)";
+          };
+        };
+      };
+    };
   };
 }
