0e86d39f391337c65fcc1030be3a27410360661b
elythh <gwenchlan@tuta.io>
2023-11-05 13:53:59 +0100

    :pencil2: stardew colorscheme

---
+++ home/shared/cols/stardew.nix
@@ -0,0 +1,45 @@
+{}:
+rec {
+  name = "stardewnight";
+  wallpaper = "satellitee.jpeg";
+
+  mbg = "16161a";
+  bg2 = "15181b";
+  cursorColor = "dbdfdf";
+
+  contrast = "111111";
+  comment = "676778";
+  background = "16161a";
+  black = "16161a";
+
+  darker = "15181b";
+  foreground = "e9ecf2";
+  cursorline = "212126";
+  cursor = "e9ecf2";
+
+  color0 = "2a2a30";
+  color1 = "f25c5c";
+
+  color2 = "55b682";
+  color3 = "ff9c6a";
+
+  color4 = "7aaaff";
+  color5 = "f17ac6";
+
+  color6 = "7acaff";
+  color7 = "DCDFE4";
+
+  color8 = "2a2a30";
+  color9 = "f25c5c";
+
+  color10 = "55b682";
+  color11 = "ff9c6a";
+
+  color12 = "7aaaff";
+  color13 = "f17ac6";
+
+  color14 = "7acaff";
+  color15 = "DCDFE4";
+
+  neofetchpic = "pattern.png";
+}
