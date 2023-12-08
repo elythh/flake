2221da00ea5e91afbc140fe8b59599bbc9478ef2
elythh <gwenchlan@tuta.io>
2023-12-08 18:48:53 +0100

    :wrench: zellij plugins and zsh

--- home/gwen/conf/term/zellij/default.nix
+++ home/gwen/conf/term/zellij/default.nix
@@ -4,20 +4,60 @@ with config.colorscheme.colors; {
   programs.zellij = {
     enable = true;
     enableZshIntegration = false;
-    settings = {
-      themes.default = import ./colors.nix { inherit config nix-colors; };
-      default_layout = "layout";
-      mouse_mode = true;
-      copy_on_select = true;
-      simplified_ui = false;
-      scrollback_editor = "/home/gwen/.nix-profile/bin/nvim";
-      pane_frames = false;
-      on_force_close = "detach";
-      keybinds = {
-        unbind = [ "Ctrl b" " Ctrl h" "Ctrl g" ];
-      };
-    };
   };
+  xdg.configFile."zellij/config.kdl".text = ''
+       default_layout "layout"
+mouse_mode true
+copy_on_select true
+simplified_ui  false
+scrollback_editor "/home/gwen/.nix-profile/bin/nvim"
+pane_frames false
+on_force_close "detach"
+
+keybinds {
+        normal {
+                bind "Alt m" {
+                        LaunchPlugin "file:~/.config/zellij/plugins/monocle.wasm" {
+                                in_place true
+                                kiosk true
+                        };
+                        SwitchToMode "Normal"
+                }
+                bind "Ctrl f" {
+                        LaunchOrFocusPlugin "file:~/.config/zellij/plugins/monocle.wasm" {
+                                floating true
+                        }
+                        SwitchToMode "Normal"
+                }
+        }
+
+        shared_except "locked" {
+                bind "Ctrl y" {
+                        LaunchOrFocusPlugin "file:~/.config/zellij/plugins/room.wasm" {
+                                floating true
+                                ignore_case true
+                        }
+                }
+        }
+      unbind "Ctrl h" "Ctrl b" "Ctrl g"
+    }
+
+    themes {
+      default {
+       bg  "#${background}"
+       fg  "#${foreground}"
+       black  "#${background}"
+       red  "#${color9}"
+       green  "#${color4}"
+       yellow  "#${color3}"
+       blue  "#${color4}"
+       magenta  "#${color5}"
+       cyan  "#${color6}"
+       white  "#${color7}"
+       orange  "#${color11}"
+     }
+   }
+  '';
   xdg.configFile."zellij/layouts/layout.kdl".text = '' 
         layout {
             pane split_direction="vertical" {
