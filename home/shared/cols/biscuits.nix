616652cf5de63dddd5a7482ff18007d636078738
elythh <gwenchlan@tuta.io>
2023-12-14 10:44:30 +0100

    :pencil2: biscuits

---
+++ home/shared/cols/biscuits.nix
@@ -0,0 +1,26 @@
+{}:
+rec {
+
+  wallpaper = "random.png";
+  darker = "0e0c0c";
+  background = "181515";
+  bg2 = "2d2424";
+  mbg = "352a2a";
+  foreground = "ffe9c7";
+
+  color1 = "CF223E";
+  color9 = "b61d36";
+  color2 = "959A6B";
+  color12 = "84885d";
+  color3 = "E39C45";
+  color11 = "de8b26";
+  color4 = "4A5A8D";
+  color12 = "414f7c";
+  color5 = "C45497";
+  color7 = "b73f87";
+
+  color6 = "614F76";
+  color14 = "554567";
+  name = "biscuits";
+  neofetchpic = "thecat.png";
+}
