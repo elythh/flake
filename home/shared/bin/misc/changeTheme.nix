093ee4278db5c926e50b2c8d87b4f6bbed0e1eaf
elythh <gwenchlan@tuta.io>
2024-01-03 14:08:45 +0100

    :pencil2: changeTheme fix

--- home/shared/bin/misc/changeTheme.nix
+++ home/shared/bin/misc/changeTheme.nix
@@ -2,7 +2,7 @@ _:
 ''
   #!/usr/bin/env sh
   THEME=$1
-  sed -i "5s/.*/  colors = import ..\/shared\/cols\/$THEME.nix { };/g" /etc/nixos/home/gwen/home.nix
+  sed -i "/theme = */c\theme = $THEME;" /etc/nixos/home/gwen/home.nix
   cd /etc/nixos && home-manager switch --flake ".#$USER@thinkpad"
   echo $THEME > /tmp/themeName
   awesome-client 'awesome.emit_signal("colors::refresh")'
