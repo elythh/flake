9192176f0a917d5459c3b9a2bd3babb91c84c035
elythh <gwenchlan@tuta.io>
2024-01-08 16:52:53 +0100

    :zap: wallpaper option

---
+++ home/gwen/misc/wallpaper.nix
@@ -0,0 +1,13 @@
+{ pkgs, lib, ... }:
+let
+  inherit (lib) types mkOption;
+in
+{
+  options.wallpaper = mkOption {
+    type = types.path;
+    default = "${pkgs.nixos-artwork.wallpapers.nineish-dark-gray}/share/backgrounds/nixos/nix-wallpaper-nineish-dark-gray.png";
+    description = ''
+      Wallpaper path
+    '';
+  };
+}
