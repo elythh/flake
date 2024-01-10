d4d8a30f863dcd64884a07716f698171a4b94d59
elythh <gwenchlan@tuta.io>
2024-01-11 00:29:01 +0100

    :wrench: default mimes

--- home/gwen/conf/ui/hyprland/default.nix
+++ home/gwen/conf/ui/hyprland/default.nix
@@ -85,6 +85,18 @@ in
     enable = true;
     cacheHome = config.home.homeDirectory + "/.cache";
     mimeApps.enable = true;
+    mimeApps.defaultApplications = {
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https " = " firefox.desktop";
+      "x-scheme-handler/chrome" = "firefox.desktop";
+      "text/html " = " firefox.desktop";
+      "application/x-extension-htm" = "firefox.desktop";
+      "application/x-extension-html " = " firefox.desktop";
+      "application/x-extension-shtml" = "firefox.desktop";
+      "application/xhtml+xml " = " firefox.desktop";
+      "application/x-extension-xhtml" = "firefox.desktop";
+      "application/x-extension-xht " = " firefox.desktop";
+    };
     userDirs = {
       enable = true;
       createDirectories = true;
