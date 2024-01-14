7a224ae1cd1c464731d5110186efd4083e4470e1
elythh <gwenchlan@tuta.io>
2024-01-14 02:55:03 +0100

    🩹 changeTheme update for hyprland

--- home/shared/bin/misc/changeTheme.nix
+++ home/shared/bin/misc/changeTheme.nix
@@ -5,5 +5,5 @@ _:
   sed -i "/theme = */c\theme = \"$THEME\";" /etc/nixos/home/gwen/home.nix
   cd /etc/nixos && home-manager switch --flake ".#$USER@thinkpad"
   echo $THEME > /tmp/themeName
-  # awesome-client 'awesome.emit_signal("colors::refresh")'
+  systemctl --user restart waybar.service swaybg.service
 ''
