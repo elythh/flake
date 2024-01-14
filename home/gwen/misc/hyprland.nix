9c89f3959f86ade761e4f6e11ebc0ed09b87deea
elythh <gwenchlan@tuta.io>
2024-01-14 13:55:36 +0100

    ✨ hyprland misc

---
+++ home/gwen/misc/hyprland.nix
@@ -0,0 +1,135 @@
+{ lib, pkgs, config, inputs, themes, ... }: {
+  imports = [
+    inputs.hyprland.nixosModules.default
+  ];
+
+  environment = {
+    variables = {
+      NIXOS_OZONE_WL = "1";
+      _JAVA_AWT_WM_NONEREPARENTING = "1";
+      GDK_BACKEND = "wayland,x11";
+      GTK_USE_PORTAL = "1";
+      MOZ_ENABLE_WAYLAND = "1";
+      XDG_SESSION_TYPE = "wayland";
+      SDL_VIDEODRIVER = "wayland";
+      CLUTTER_BACKEND = "wayland";
+      QT_QPA_PLATFORM = "wayland;xcb";
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    };
+
+    systemPackages = with pkgs; [
+      libsForQt5.dolphin
+      libsForQt5.ark
+      libsForQt5.gwenview
+      libsForQt5.dolphin-plugins
+      libsForQt5.ffmpegthumbs
+      libsForQt5.kdegraphics-thumbnailers
+      libsForQt5.kimageformats
+      libsForQt5.kio
+      libsForQt5.kio-extras
+      libsForQt5.qtwayland
+      libsForQt5.okular
+    ];
+  };
+
+  nixpkgs.overlays = [
+    inputs.nixpkgs-wayland.overlay
+  ];
+
+  nix.settings = {
+    substituters = [
+      "https://hyprland.cachix.org"
+      "https://anyrun.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+    ];
+  };
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  home-manager.sharedModules = [
+    inputs.ags.homeManagerModules.default
+    inputs.anyrun.homeManagerModules.default
+    inputs.hyprland.homeManagerModules.default
+    inputs.self.homeModules.hyprland
+  ];
+
+  xdg.portal = {
+    enable = true;
+    config.common.default = "*";
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  security = {
+    pam.services = {
+      greetd = {
+        gnupg.enable = true;
+        enableGnomeKeyring = true;
+      };
+
+      login = {
+        enableGnomeKeyring = true;
+        gnupg = {
+          enable = true;
+          noAutostart = true;
+          storeOnly = true;
+        };
+      };
+
+      swaylock.text = "auth include login";
+    };
+
+    polkit.enable = true;
+  };
+
+  services = {
+    gnome = {
+      gnome-keyring.enable = true;
+      glib-networking.enable = true;
+    };
+
+    greetd = {
+      enable = true;
+      settings = {
+        terminal.vt = 1;
+        default_session =
+          let
+            base = config.services.xserver.displayManager.sessionData.desktops;
+          in
+          {
+            command = lib.concatStringsSep " " [
+              (lib.getExe pkgs.greetd.tuigreet)
+              "--time"
+              "--remember"
+              "--remember-user-session"
+              "--asterisks"
+              "--sessions '${base}/share/wayland-sessions:${base}/share/xsessions'"
+            ];
+            user = "greeter";
+          };
+      };
+    };
+  };
+
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+}
