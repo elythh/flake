0f216270f567be298ab424b963d9700874213960
elythh <gwenchlan@tuta.io>
2024-01-14 02:47:33 +0100

    🎨 format

--- home/gwen/conf/ui/hyprland/services/cliphist.nix
+++ home/gwen/conf/ui/hyprland/services/cliphist.nix
@@ -1,18 +1,16 @@
+{ lib, pkgs, ... }:
 {
-  lib,
-  pkgs,
-  ...
-}: {
   systemd.user.services.cliphist = {
     Unit = {
       Description = "Clipboard history";
-      PartOf = ["graphical-session.target"];
-      After = ["graphical-session.target"];
+      PartOf = [ "graphical-session.target" ];
+      After = [ "graphical-session.target" ];
     };
     Service = {
       ExecStart = "${pkgs.wl-clipboard}/bin/wl-paste --watch ${lib.getBin pkgs.cliphist}/cliphist store";
       Restart = "on-failure";
     };
-    Install.WantedBy = ["graphical-session.target"];
+    Install.WantedBy = [ "graphical-session.target" ];
   };
 }
+
