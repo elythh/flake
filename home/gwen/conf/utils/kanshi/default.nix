d06b49d9509415dc2e3c1c5e928dc89805c90d51
elythh <gwenchlan@tuta.io>
2024-01-29 13:43:10 +0100

    🐛 kanshi: use monitors name instead

--- home/gwen/conf/utils/kanshi/default.nix
+++ home/gwen/conf/utils/kanshi/default.nix
@@ -5,15 +5,21 @@
 
     profile {
     	output eDP-1 disable
-      output HDMI-A-1 mode 1920x1080 position 2560,0
-      output DP-5 mode 2560x1440@60 position 0,0
+      output "Philips Consumer Electronics Company 247ELH AU01243009967" mode 1920x1080 position 2560,0
+      output "XMI Mi Monitor 3342300023271" mode 2560x1440@60 position 0,0
     }
     
     profile {
       output eDP-1 disable
       output "BNQ BenQ BL2480 JBL0008901Q" mode 1920x1080@60 position 1920,0
       output "BNQ BenQ BL2480 RAL0223001Q" mode 1920x1080@60 position 0,0
-   }
+    }
+
+    profile {
+      output eDP-1 disable
+      output "BNQ BenQ BL2480 JBL0040201Q" mode 1920x1080 position 0,0
+      output "Fujitsu Siemens Computers GmbH B22T-7 LED PG YV6J068403" mode 1920x1080 position 1920,0
+    }
 
    profile {
      output eDP-1 mode 1920x1080 position 0,0
