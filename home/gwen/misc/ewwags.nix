5405c22f127875e1da780a4a716c5b70dbb1151e
elythh <gwenchlan@tuta.io>
2024-02-01 00:58:02 +0100

    🔧 ags: move color config

--- home/gwen/misc/ewwags.nix
+++ home/gwen/misc/ewwags.nix
@@ -26,23 +26,6 @@ with config.colorscheme.colors ;{
     $magenta : #${color5};
     $magenta-light : #${color13};
 
-    $comment : #${comment};
-    $accent : #${accent};
-
-  '';
-  home.file.".config/ags/style/_colors.scss".text = ''
-    $background : #${background};
-    $foreground : #${foreground};
-    $background-alt : #${mbg};
-    $background-light : #${color0};
-    $foreground-alt : #${color7};
-    $red : #${color1};
-    $red-light : #${color9};
-
-    $green : #${color2};
-    $green-light : #${color10};
-
-
     $yellow : #${color3};
     $yellow-light : #${color11};
 
