e2e8a5518306ae5df02a4848573a7e17187df7b7
elythh <gwenchlan@tuta.io>
2024-02-01 00:44:29 +0100

    ✨ sss

---
+++ home/gwen/conf/utils/sss/default.nix
@@ -0,0 +1,26 @@
+{ config, inputs, ... }:
+{
+  imports = [
+    inputs.sss.nixosModules.home-manager
+  ];
+
+  programs.sss = {
+    enable = true;
+
+    # General Config
+    general = with config.colorscheme.colors;{
+      author = "gwen";
+      # copy = true;
+      colors = {
+        background = "#${accent}";
+        author = "#${foreground}";
+        shadow = "#${darker}";
+      };
+      fonts = "Product Sans=12.0";
+      radius = 8;
+      save-format = "png";
+      shadow = true;
+      shadow-image = true;
+    };
+  };
+}
