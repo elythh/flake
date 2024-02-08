8673a42d7faf2a26181e20dc0a3005179bdac8aa
elythh <gwenchlan@tuta.io>
2024-02-08 17:33:47 +0100

    :bug: bluetooth is broken :) idk how to fix

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -62,8 +62,6 @@ in
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    audio.enable = true;
-    jack.enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
   };
