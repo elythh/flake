dcc62ee2d2574a8c2f9dc791d617cd20d1b77d04
elythh <gwenchlan@tuta.io>
2024-02-16 23:50:20 +0100

    :art: waybar
    Idk how to make it work with split-workspace-monitor

--- home/gwen/conf/ui/wayland/programs/waybar/default.nix
+++ home/gwen/conf/ui/wayland/programs/waybar/default.nix
@@ -28,13 +28,22 @@
 
         # Workspaces
         "hyprland/workspaces" = {
-          format = "";
+          format = " {icon} ";
           on-click = "activate";
           disable-scroll = true;
           all-outputs = false;
           show-special = true;
-          persistent-workspaces = {
-            "*" = 5;
+          format-icons = {
+            "11" = "1";
+            "12" = "2";
+            "13" = "3";
+            "14" = "4";
+            "15" = "5";
+            "16" = "6";
+            "17" = "7";
+            "18" = "8";
+            "19" = "9";
+            "20" = "10";
           };
         };
 
