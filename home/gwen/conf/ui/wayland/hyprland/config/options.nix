cd52a1059f8fd261f328a208ba577c4aaeacd20c
elythh <gwenchlan@tuta.io>
2024-02-19 22:50:03 +0100

    :wrench: hyprland: bullshit

--- home/gwen/conf/ui/wayland/hyprland/config/options.nix
+++ home/gwen/conf/ui/wayland/hyprland/config/options.nix
@@ -7,8 +7,7 @@
         "wl-paste --type text --watch cliphist store"
         "wl-paste --type image --watch cliphist store"
         "xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 1"
-        "echo 'Xft.dpi: 130' | xrdb -merge"
-        "xwaylandvideobridge &"
+        #"xwaylandvideobridge &"
       ];
       animations = {
         enabled = true;
