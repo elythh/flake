708864fd9bce9ebd3a7b9d82ab7b11652b95594c
elythh <gwenchlan@tuta.io>
2024-02-20 21:45:51 +0100

    :wrench: kanshi: use home-manager instead

--- home/gwen/conf/ui/wayland/programs/kanshi/default.nix
+++ home/gwen/conf/ui/wayland/programs/kanshi/default.nix
@@ -8,39 +8,83 @@
     ];
 
   };
-  home.file.".config/kanshi/config".text = ''
+  services.kanshi = {
+    enable = true;
+    systemdTarget = "hyprland-session.target";
+    profiles = {
+      home = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "Philips Consumer Electronics Company 247ELH AU01243009967";
+            mode = "1920x1080";
+            position = "2560,0";
+          }
+          {
+            criteria = "XMI Mi Monitor 3342300023271";
+            mode = "2560x1440@60";
+            position = "0,0";
+          }
+        ];
+      };
+      work_up = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "BNQ BenQ BL2480 JBL0008901Q";
+            mode = "1920x1080@60";
+            position = "1920,0";
+          }
+          {
+            criteria = "BNQ BenQ BL2480 RAL0223001Q";
+            mode = "1920x1080@60";
+            position = "0,0";
+          }
+        ];
+      };
+      work_end = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "BNQ BenQ BL2480 JBL0040201Q";
+            mode = "1920x1080";
+            position = "0,0";
+          }
+          {
+            criteria = "Fujitsu Siemens Computers GmbH B22T-7 LED PG YV6J068403";
+            mode = "1920x1080";
+            position = "1920,0";
+          }
+        ];
+      };
+      work_middle = {
+        outputs = [
+          {
+            criteria = "eDP-1";
+            status = "disable";
+          }
+          {
+            criteria = "BNQ BenQ BL2480 JBL0040201Q";
+            mode = "1920x1080";
+            position = "0,0";
+          }
+          {
+            criteria = "Fujitsu Siemens Computers GmbH B22T-7 LED PG YV6J068403";
+            mode = "1920x1080";
+            position = "1920,0";
+          }
+        ];
+      };
+    };
 
-    # maison
-    profile {
-    	output eDP-1 disable
-      output "Philips Consumer Electronics Company 247ELH AU01243009967" mode 1920x1080 position 2560,0
-      output "XMI Mi Monitor 3342300023271" mode 2560x1440@60 position 0,0
-    }
-    
-    # en face de francois
-    profile {
-      output eDP-1 disable
-      output "BNQ BenQ BL2480 JBL0008901Q" mode 1920x1080@60 position 1920,0
-      output "BNQ BenQ BL2480 RAL0223001Q" mode 1920x1080@60 position 0,0
-    }
-
-    # place du fond
-    profile {
-      output eDP-1 disable
-      output "BNQ BenQ BL2480 JBL0040201Q" mode 1920x1080 position 0,0
-      output "Fujitsu Siemens Computers GmbH B22T-7 LED PG YV6J068403" mode 1920x1080 position 1920,0
-    }
-    
-    # place Anto
-    profile {
-      output eDP-1 disable
-      output "Ancor Communications Inc ASUS VS229 FALMQS045053"  mode 1920x1080 position 0,0
-      output "Ancor Communications Inc ASUS VS229 EBLMQS089619" mode 1920x1080 position 1920,0
-    }
-
-    # default
-   profile {
-     output eDP-1 mode 1920x1080 position 0,0
-   }
-  '';
+  };
 }
