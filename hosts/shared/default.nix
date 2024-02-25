3dd426c5392886e6edd9904e7ef7425b4917fb2f
elythh <gwenchlan@tuta.io>
2024-02-25 16:10:23 +0100

    :wrench: fmt

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -172,19 +172,21 @@ in {
       enable = true;
       settings = {
         terminal.vt = 1;
-        default_session = let
-          base = config.services.xserver.displayManager.sessionData.desktops;
-        in {
-          command = lib.concatStringsSep " " [
-            (lib.getExe pkgs.greetd.tuigreet)
-            "--time"
-            "--remember"
-            "--remember-user-session"
-            "--asterisks"
-            "--sessions '${base}/share/wayland-sessions:${base}/share/xsessions'"
-          ];
-          user = "greeter";
-        };
+        default_session =
+          let
+            base = config.services.xserver.displayManager.sessionData.desktops;
+          in
+          {
+            command = lib.concatStringsSep " " [
+              (lib.getExe pkgs.greetd.tuigreet)
+              "--time"
+              "--remember"
+              "--remember-user-session"
+              "--asterisks"
+              "--sessions '${base}/share/wayland-sessions:${base}/share/xsessions'"
+            ];
+            user = "greeter";
+          };
       };
     };
   };
