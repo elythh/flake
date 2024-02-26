2b4da65bec7c99a0add6ed474d048b8326946213
elythh <gwenchlan@tuta.io>
2024-02-27 00:24:07 +0100

    :lipstick: lint/fmt

--- home/gwen/home.nix
+++ home/gwen/home.nix
@@ -193,12 +193,10 @@
     (final: prev: {
       # Fix slack screen sharing following: https://github.com/flathub/com.slack.Slack/issues/101#issuecomment-1807073763
       slack = prev.slack.overrideAttrs (previousAttrs: {
-        installPhase =
-          previousAttrs.installPhase
-          + ''
-            sed -i'.backup' -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
+        installPhase = previousAttrs.installPhase + ''
+          sed -i'.backup' -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
 
-          '';
+        '';
       });
     })
 
