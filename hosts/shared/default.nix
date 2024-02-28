7ed2c834ebe35edf8590e6b8fb8f0958b854d377
elythh <gwenchlan@tuta.io>
2024-02-28 22:31:20 +0100

    :lipstick: (fmt): YOU ARE ANNOYING NIXFMT

--- hosts/shared/default.nix
+++ hosts/shared/default.nix
@@ -173,21 +173,19 @@ in {
       enable = true;
       settings = {
         terminal.vt = 1;
-        default_session =
-          let
-            base = config.services.xserver.displayManager.sessionData.desktops;
-          in
-          {
-            command = lib.concatStringsSep " " [
-              (lib.getExe pkgs.greetd.tuigreet)
-              "--time"
-              "--remember"
-              "--remember-user-session"
-              "--asterisks"
-              "--sessions '${base}/share/wayland-sessions:${base}/share/xsessions'"
-            ];
-            user = "greeter";
-          };
+        default_session = let
+          base = config.services.xserver.displayManager.sessionData.desktops;
+        in {
+          command = lib.concatStringsSep " " [
+            (lib.getExe pkgs.greetd.tuigreet)
+            "--time"
+            "--remember"
+            "--remember-user-session"
+            "--asterisks"
+            "--sessions '${base}/share/wayland-sessions:${base}/share/xsessions'"
+          ];
+          user = "greeter";
+        };
       };
     };
   };
