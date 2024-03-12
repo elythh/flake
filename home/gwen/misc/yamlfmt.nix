98d4d25ddc4f2b81fdc95ddb7d13fbb5b37f7c07
elythh <gwenchlan@tuta.io>
2024-03-12 14:00:45 +0100

    :sparkles: (yamlfmt): config

---
+++ home/gwen/misc/yamlfmt.nix
@@ -0,0 +1,10 @@
+{
+  home.file.".config/yamlfmt/.yamlfmt".text = ''
+    formatter:
+      type: basic
+      disallow_anchors: false
+      include_document_start: false
+      retain_line_breaks: true
+      indentless_arrays: true
+  '';
+}
