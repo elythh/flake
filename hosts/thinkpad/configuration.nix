4f136bb011d2752dfc056c1e1f6bf7b87a1c3bc0
elythh <gwenchlan@tuta.io>
2024-03-13 21:19:59 +0100

    :sparkles: steam

--- hosts/thinkpad/configuration.nix
+++ hosts/thinkpad/configuration.nix
@@ -13,6 +13,12 @@
     ../shared
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+
   nixpkgs = {
     overlays = [
       inputs.nur.overlay
