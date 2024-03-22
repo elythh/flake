de251912e116437908216e929cc076c019a9986c
elythh <gwenchlan@tuta.io>
2024-03-23 00:23:51 +0100

    :wrench: (hyprland): enable swallowing

--- home/gwen/conf/ui/wayland/hyprland/config/options.nix
+++ home/gwen/conf/ui/wayland/hyprland/config/options.nix
@@ -93,6 +93,8 @@
     };
 
     misc = {
+      enable_swallow = true; # hide windows that spawn other windows
+      swallow_regex = "thunar|nemo|wezterm"; # windows for which swallow is applied
       disable_autoreload = false;
       disable_hyprland_logo = true;
       disable_splash_rendering = true;
