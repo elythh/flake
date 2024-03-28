800ef47b2c12433fa2ffd5f44022aaa83408ebcc
elythh <gwenchlan@tuta.io>
2024-03-28 17:55:07 +0100

    :wrench: (work): yes i want steam
    to play balatro

--- hosts/hp/configuration.nix
+++ hosts/hp/configuration.nix
@@ -5,6 +5,11 @@
     ../shared
   ];
 
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
   services = {
     logind = {
       powerKey = "suspend";
