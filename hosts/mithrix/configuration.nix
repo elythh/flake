562cb340df8d783910f1b58ab5606c7d5b4d5299
elythh <gwenchlan@tuta.io>
2024-05-21 02:47:53 +0200

    :sparkles: (host): mithrix
    homelab

---
+++ hosts/mithrix/configuration.nix
@@ -0,0 +1,33 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{pkgs, ...}: {
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  networking.hostName = "mithrix";
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.mithrix = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "docker"]; # Enable ‘sudo’ for the user.
+
+    packages = with pkgs; [
+      firefox
+      tree
+    ];
+  };
+  virtualisation.docker.enable = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  system.stateVersion = "24.05"; # Did you read the comment?
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+}
