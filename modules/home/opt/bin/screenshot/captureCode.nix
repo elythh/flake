6a523718ba981334e880b824e87b0316c69f5d5d
elythh <gwenchlan@tuta.io>
2024-06-02 10:48:13 +0200

    :lipstick: only use stylix

--- modules/home/opt/bin/screenshot/captureCode.nix
+++ modules/home/opt/bin/screenshot/captureCode.nix
@@ -1,5 +1,5 @@
 {config, ...}: let
-  color = config.colorscheme.palette;
+  color = config.lib.stylix.colors;
 in ''
-  wl-paste | sss_code --window-controls --window-title "code moche" -n -e yaml --background '#${color.accent}' -f png -o raw - | satty --early-exit -f - --copy-command wl-copy --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
+  wl-paste | sss_code --window-controls --window-title "code moche" -n -e yaml --background '#${color.base06}' -f png -o raw - | satty --early-exit -f - --copy-command wl-copy --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
 ''
--- modules/home/opt/shell/zsh/default.nix
+++ modules/home/opt/shell/zsh/default.nix
@@ -6,50 +6,13 @@
   imports = [./run-as-service.nix];
 
   config = lib.mkIf config.modules.zsh.enable {
-    programs.zsh = with config.colorscheme.palette; {
+    programs.zsh = {
       enable = true;
       dotDir = ".config/zsh";
       envExtra = ''
         export PATH=~/.local/bin:~/.local/share/nvim/mason/bin:$PATH
       '';
       initExtra = ''
-        background="#${background}"
-        foreground="#${foreground}"
-        mbg="#${mbg}"
-        darker="#${darker}"
-        accent="#${accent}"
-
-        # Black
-        color0="#${color0}"
-        color8="#${color0}"
-
-        # Red
-        color1="#${color1}"
-        color9="#${color9}"
-
-        # Green
-        color2="#${color2}"
-        color10="#${color10}"
-
-        # Yellow
-        color3="#${color3}"
-        color11="#${color11}"
-
-        # Blue
-        color4="#${color4}"
-        color12="#${color12}"
-
-        # Magenta
-        color5="#${color5}"
-        color13="#${color13}"
-
-        # Cyan
-        color6="#${color6}"
-        color14="#${color14}"
-        # White
-        color7="#${color7}"
-        color15="#${color15}"
-
         source ~/.config/zsh/env.zsh
         source ~/.config/zsh/aliases.zsh
         source ~/.config/zsh/options.zsh
@@ -66,7 +29,7 @@
       '';
     };
 
-    programs.starship = with config.colorscheme.palette; {
+    programs.starship = with config.lib.stylix.colors; {
       enable = true;
       settings = {
         format = "$directory$nix_shell$fill$git_branch$kubernetes$git_status$cmd_duration$line_break$character";
@@ -87,23 +50,23 @@
         fill = {symbol = " ";};
         conda = {format = " [ $symbol$environment ] (dimmed green) ";};
         character = {
-          success_symbol = "[ ](#${accent} bold)";
-          error_symbol = "[ ](#${color1} bold)";
-          vicmd_symbol = "[](#${color3})";
+          success_symbol = "[ ](#${base03} bold)";
+          error_symbol = "[ ](#${base04} bold)";
+          vicmd_symbol = "[](#${base03})";
         };
         directory = {
-          format = "[]($style)[  ](bg:#${darker} fg:#${accent})[$path](bg:#${darker} fg:#${color7} bold)[ ]($style)";
-          style = "bg:none fg:#${darker}";
+          format = "[]($style)[  ](bg:#${base01} fg:#${base05})[$path](bg:#${base01} fg:#${base09} bold)[ ]($style)";
+          style = "bg:none fg:#${base01}";
           truncation_length = 3;
           truncate_to_repo = false;
         };
         git_branch = {
-          format = "[]($style)[[ ](bg:#${darker} fg:#${accent} bold)$branch](bg:#${darker} fg:#${color7} bold)[ ]($style)";
-          style = "bg:none fg:#${darker}";
+          format = "[]($style)[[ ](bg:#${base01} fg:#${base05} bold)$branch](bg:#${base01} fg:#${base09} bold)[ ]($style)";
+          style = "bg:none fg:#${base01}";
         };
         git_status = {
-          format = "[]($style)[$all_status$ahead_behind](bg:#${darker} fg:#${color7} bold)[ ]($style)";
-          style = "bg:none fg:#${darker}";
+          format = "[]($style)[$all_status$ahead_behind](bg:#${base01} fg:#${base09} bold)[ ]($style)";
+          style = "bg:none fg:#${base01}";
           conflicted = "=";
           ahead = "⇡\${count}";
           behind = "⇣\${count} ";
@@ -119,23 +82,23 @@
         cmd_duration = {
           min_time = 1;
           # duration & style ;
-          format = "[]($style)[[  ](bg:#${darker} fg:#${accent} bold)$duration](bg:#${darker} fg:#${color7} bold)[]($style)";
+          format = "[]($style)[[  ](bg:#${base01} fg:#${base05} bold)$duration](bg:#${base01} fg:#${base09} bold)[]($style)";
           disabled = false;
-          style = "bg:none fg:#${darker}";
+          style = "bg:none fg:#${base01}";
         };
         nix_shell = {
           disabled = false;
           heuristic = false;
-          format = "[]($style)[ ](bg:#${darker} fg:#${accent} bold)[]($style)";
-          style = "bg:none fg:#${darker}";
+          format = "[]($style)[ ](bg:#${base01} fg:#${base05} bold)[]($style)";
+          style = "bg:none fg:#${base01}";
           impure_msg = "";
           pure_msg = "";
           unknown_msg = "";
         };
         kubernetes = {
-          format = "[](fg:#${darker} bg:none)[  ](fg:#${accent} bg:#${darker})[$context/$namespace]($style)[](fg:#${darker} bg:none) ";
+          format = "[](fg:#${base01} bg:none)[  ](fg:#${base05} bg:#${base01})[$context/$namespace]($style)[](fg:#${base01} bg:none) ";
           disabled = false;
-          style = "fg:#${color7} bg:#${darker} bold";
+          style = "fg:#${base09} bg:#${base01} bold";
           context_aliases = {
             "dev.local.cluster.k8s" = "dev";
             ".*/openshift-cluster/.*" = "openshift";
