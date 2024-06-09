681b8d02f9674e10656163e04d90f23fe3ff7178
elythh <gwenchlan@tuta.io>
2024-06-09 16:48:22 +0200

    :wrench: (nvim): enable obsidian

--- modules/home/opt/nvim/nixvim.nix
+++ modules/home/opt/nvim/nixvim.nix
@@ -5,7 +5,10 @@
 }: let
   nixvim' = inputs.nixvim.packages."x86_64-linux".default;
   nvim = nixvim'.nixvimExtend {
-    config.theme = lib.mkForce "paradise";
+    config = {
+      theme = lib.mkForce "paradise";
+      plugins.obsidian.enable = lib.mkForce true;
+    };
   };
 in {
   home.packages = [
