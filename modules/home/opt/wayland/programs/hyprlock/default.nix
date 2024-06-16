120441565d80e0912dc8886ad6f2728bafc31ace
elythh <gwenchlan@tuta.io>
2024-06-16 18:32:17 +0200

    :wrench: (hyprlock): settings

--- modules/home/opt/wayland/programs/hyprlock/default.nix
+++ modules/home/opt/wayland/programs/hyprlock/default.nix
@@ -1,115 +1,63 @@
 {
-  inputs,
   config,
   lib,
   ...
 }: {
   config = lib.mkIf (config.default.lock == "hyprlock" && config.default.de == "hyprland") {
-    programs.hyprlock = {
+    programs.hyprlock = with config.lib.stylix.colors; {
       enable = true;
 
       settings = {
         general = {
-          disable_loading_bar = false;
+          disable_loading_bar = true;
+          grace = 300;
           hide_cursor = true;
-          grace = 2;
           no_fade_in = false;
         };
-        backgrounds = [
-          {
-            path = "${config.wallpaper}";
 
-            # Blur
-            blur_passes = 7; # 0 disables blurring
+        background = [
+          {
+            path = "screenshot";
+            blur_passes = 3;
             blur_size = 8;
-            noise = 1.17e-2;
-            contrast = 0.8916;
-            brightness = 0.8172;
-            vibrancy = 0.1696;
-            vibrancy_darkness = 0.0;
           }
         ];
 
-        input-fields = [
+        input-field = [
           {
-            size = {
-              width = 300;
-              height = 50;
-            };
-            outline_thickness = 3;
-            dots_size = 0.33;
-            dots_spacing = 0.15;
+            size = "200, 50";
+            position = "0, -90";
+            monitor = "";
             dots_center = true;
-            outer_color = "rgba(255, 255, 255, 0.1)";
-            inner_color = "rgba(255, 255, 255, 0.1)";
-            font_color = "rgb(255, 255, 255)";
-            fade_on_empty = true;
-            placeholder_text = "<i>Input Password...</i>";
-            hide_input = false;
-            position = {
-              x = 0;
-              y = -100;
-            };
-            halign = "center";
-            valign = "center";
+            fade_on_empty = false;
+            font_color = "${base05}";
+            inner_color = "${base01}";
+            outer_color = "${base01}";
+            outline_thickness = 5;
+            placeholder_text = "Password...";
+            shadow_passes = 2;
           }
         ];
-
-        labels = [
+        # Time
+        label = [
           {
-            text = "<b>$TIME</b>";
-            color = "rgb(255, 255, 255)";
+            text = "cmd[update:1000] echo \"$(date +'%-I:%M%p')\"";
+            color = "${base05}";
             font_size = 120;
-            position = {
-              x = 0;
-              y = -40;
-            };
+            font_family = "JetBrains Mono Nerd Font Mono ExtraBold";
+            position = "0, -300";
             halign = "center";
             valign = "top";
           }
+          # User
           {
-            text = ''cmd[update:1000] echo "$(date "+%a %d %b")"'';
-            color = "rgb(255, 255, 255)";
-            font_size = 24;
-            position = {
-              x = 0;
-              y = -220;
-            };
-            halign = "center";
-            valign = "top";
-          }
-          {
-            text = "Don't touch !";
-            color = "rgb(255, 255, 255)";
-            font_size = 32;
-            position = {
-              x = 0;
-              y = 0;
-            };
+            text = "Hi there, $USER";
+            color = "${base05}";
+            font_size = 25;
+            font_family = "JetBrains Mono Nerd Font Mono";
+            position = "0, -40";
             halign = "center";
             valign = "center";
-          }
-          {
-            text = "Unless your name is <i>$USER</i>";
-            color = "rgb(255, 255, 255)";
-            font_size = 32;
-            position = {
-              x = 0;
-              y = -43;
-            };
-            halign = "center";
-            valign = "center";
-          }
-          {
-            text = "cmd[] echo ' '";
-            color = "rgb(255, 255, 255)";
-            font_size = 50;
-            position = {
-              x = 0;
-              y = 0;
-            };
-            halign = "center";
-            valign = "bottom";
           }
         ];
       };
