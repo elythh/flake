68d82ef1b0a006d442d0c38b3409144e744cffcf
elythh <gwenchlan@tuta.io>
2024-06-17 19:43:06 +0200

    :wrench: (wezterm): zedmono is back

--- modules/home/opt/term/wezterm/default.nix
+++ modules/home/opt/term/wezterm/default.nix
@@ -21,7 +21,7 @@ lib.mkIf (config.default.terminal == "wezterm") {
         -- Fonts
         --------
         font         = wez.font_with_fallback({
-          "Fantasque Sans M Nerd Font",
+          "ZedMono NF",
           "Material Design Icons",
         }),
         dpi = 96.0,
