adf941a86310060bdd5fd51b1a33fe7c20a7afdf
elythh <gwenchlan@tuta.io>
2024-06-21 18:56:06 +0200

    :wrench: (nixvim): enable copilot and set theme as var

--- modules/home/opt/nvim/nixvim.nix
+++ modules/home/opt/nvim/nixvim.nix
@@ -1,13 +1,15 @@
 {
   inputs,
+  config,
   lib,
   ...
 }: let
   nixvim' = inputs.nixvim.packages."x86_64-linux".default;
   nvim = nixvim'.nixvimExtend {
     config = {
-      theme = lib.mkForce "paradise";
+      theme = lib.mkForce "${config.theme}";
       plugins.obsidian.enable = lib.mkForce true;
+      assistant = "copilot";
     };
   };
 in {
