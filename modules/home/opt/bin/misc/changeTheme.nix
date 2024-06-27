5114471c787e33fe3cd7c3608d4d0300494ce89d
elythh <gwenchlan@tuta.io>
2024-06-27 22:02:51 +0200

    :truck: (os): thinkpad -> grovetender
    yes i like Risk of Rain 2

--- modules/home/opt/bin/misc/changeTheme.nix
+++
@@ -1,7 +0,0 @@
-_: ''
-  #!/usr/bin/env sh
-  THEME=$1
-  sed -i "s/theme = .*/ theme  = \"$THEME\";"/1 /etc/nixos/home/gwen/home.nix
-  cd /etc/nixos && home-manager switch --flake ".#$USER@thinkpad"
-  echo $THEME > /tmp/themeName
-''
