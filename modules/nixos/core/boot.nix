90a304ab9ac19c86c7f4a8fe4d3d563d0b6b3413
elythh <gwenchlan@tuta.io>
2024-07-08 15:15:45 +0200

    :wrench: (bootloader): switch to grub2

--- modules/nixos/core/boot.nix
+++ modules/nixos/core/boot.nix
@@ -2,7 +2,19 @@
   boot = {
     kernel.sysctl."net.isoc" = true;
     loader = {
-      systemd-boot.enable = true;
+      grub2-theme = {
+        enable = true;
+        theme = "stylish";
+        footer = true;
+      };
+      grub = {
+        enable = true;
+        useOSProber = true;
+        efiSupport = true;
+        version = 2;
+        device = "nodev";
+      };
+      #systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
       efi.efiSysMountPoint = "/boot";
     };
