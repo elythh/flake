52364260cd71c4573c9781cd8bf0ce9c78cd97b5
elythh <gwenchlan@tuta.io>
2024-08-01 01:06:01 +0200

    :wrench: (nixvim): enable stylix

--- modules/home/opt/nvim/nixvim.nix
+++ modules/home/opt/nvim/nixvim.nix
@@ -7,7 +7,14 @@ let
   nixvim' = inputs.nixvim.packages."x86_64-linux".default;
   nvim = nixvim'.nixvimExtend {
     config = {
-      theme = lib.mkForce "${config.theme}";
+      colorschemes.base16 = {
+        colorscheme = lib.mkForce {
+          inherit (config.lib.stylix.colors.withHashtag)
+            base00 base01 base02 base03 base04 base05 base06 base07
+            base08 base09 base0A base0B base0C base0D base0E base0F;
+        };
+        enable = lib.mkForce true;
+      };
       plugins.obsidian.enable = lib.mkForce true;
       assistant = "copilot";
     };
