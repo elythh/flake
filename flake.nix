aefc8331ee7b8ddd5960332568aa09a0dd1b5127
elythh <gwenchlan@tuta.io>
2024-08-07 00:04:29 +0200

    :package: (flake): refactor
    and update

--- flake.nix
+++ flake.nix
@@ -5,9 +5,6 @@
     # Nixpkgs Stable
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    # Nixpkgs uunstable
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/release-23.11";
-
     # grub2 theme
     grub2-themes.url = "github:vinceliuice/grub2-themes";
     grub2-themes.inputs.nixpkgs.follows = "nixpkgs";
@@ -22,9 +19,6 @@
 
     # Nixos hardware
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    # Nix colors, used for app theming
-    #nix-colors.url = "github:misterio77/nix-colors";
 
     # Stylix, nix-colors alertnative
     stylix.url = "github:danth/stylix";
@@ -41,14 +35,12 @@
     # Ags, a customizable and extensible shell
     ags.url = "github:Aylur/ags";
 
-    # Waybar, the wayland bar
-    waybar.url = "github:/alexays/waybar";
-
     # Nix User Repository
     nur.url = "github:nix-community/NUR";
 
     # SuperScreenShot
-    sss.url = "github:/SergioRibera/sss";
+    sss.url = "github:SergioRibera/sss";
+    sss.inputs.nixpkgs.follows = "nixpkgs";
 
     # Hyprland, the modern compositor for wayland
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
@@ -72,69 +64,52 @@
     nixvim.url = "github:elythh/nixvim";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      nixpkgs-stable,
-      hm,
-      stylix,
-      nixos-hardware,
-      grub2-themes,
-      ...
-    }@inputs:
-    let
-      inherit (self) outputs;
-      system = "x86_64-linux";
-      pkgsStable = import nixpkgs-stable { inherit system; };
-    in
-    {
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
-        grovetender = nixpkgs.lib.nixosSystem {
-          modules = [
-            hm.nixosModule
-            nixos-hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen2
-            grub2-themes.nixosModules.default
-            # > Our main nixos configuration file <
-            ./hosts/grovetender/configuration.nix
-          ];
-        };
-        aurelionite = nixpkgs.lib.nixosSystem {
-          modules = [
-            hm.nixosModule
-            grub2-themes.nixosModules.default
-            ./hosts/aurelionite/configuration.nix
-          ];
-        };
-        mithrix = nixpkgs.lib.nixosSystem { modules = [ ./hosts/mithrix/configuration.nix ]; };
+  outputs = inputs: {
+    nixosConfigurations = {
+      grovetender = inputs.nixpkgs.lib.nixosSystem {
+        modules = [
+          inputs.hm.nixosModule
+          inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen2
+          inputs.grub2-themes.nixosModules.default
+
+          ./hosts/grovetender/configuration.nix
+        ];
       };
-      # Standalone home-manager configuration entrypoint
-      # Available through 'home-manager --flake .#your-username@your-hostname'
-      homeConfigurations = {
-        "gwen@grovetender" = inputs.hm.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {
-            inherit inputs pkgsStable outputs;
-          };
-          modules = [
-            # > Our main home-manager configuration file <
-            ./home/gwen/grovetender.nix
-            stylix.homeManagerModules.stylix
-          ];
+      aurelionite = inputs.nixpkgs.lib.nixosSystem {
+        modules = [
+          inputs.hm.nixosModule
+          inputs.grub2-themes.nixosModules.default
+
+          ./hosts/aurelionite/configuration.nix
+        ];
+      };
+      mithrix = inputs.nixpkgs.lib.nixosSystem { modules = [ ./hosts/mithrix/configuration.nix ]; };
+    };
+    # Standalone home-manager configuration entrypoint
+    # Available through 'home-manager --flake .#your-username@your-hostname'
+    homeConfigurations = {
+      "gwen@grovetender" = inputs.hm.lib.homeManagerConfiguration {
+        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {
+          inherit inputs;
         };
-        "gwen@aurelionite" = inputs.hm.lib.homeManagerConfiguration {
-          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-          extraSpecialArgs = {
-            inherit inputs pkgsStable outputs;
-          };
-          modules = [
-            # > Our main home-manager configuration file <
-            ./home/gwen/aurelionite.nix
-            stylix.homeManagerModules.stylix
-          ];
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home/gwen/grovetender.nix
+          inputs.stylix.homeManagerModules.stylix
+        ];
+      };
+      "gwen@aurelionite" = inputs.hm.lib.homeManagerConfiguration {
+        pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {
+          inherit inputs;
         };
+        modules = [
+          # > Our main home-manager configuration file <
+          ./home/gwen/aurelionite.nix
+          inputs.stylix.homeManagerModules.stylix
+        ];
       };
     };
+  };
 }
