d62d1779f3db438d5a0db59678b35f6d05283aed
elythh <gwenchlan@tuta.io>
2024-08-09 02:03:44 +0200

    :bug: (greetd): start hyprland

--- modules/nixos/opt/services/default.nix
+++ modules/nixos/opt/services/default.nix
@@ -44,21 +44,18 @@
         enable = true;
         settings = {
           terminal.vt = 1;
-          default_session =
-            let
-              base = config.services.displayManager.sessionData.desktops;
-            in
-            {
-              command = lib.concatStringsSep " " [
-                (lib.getExe pkgs.greetd.tuigreet)
-                "--time"
-                "--remember"
-                "--remember-user-session"
-                "--asterisks"
-                "--sessions '${base}/share/wayland-sessions:${base}/share/xsessions'"
-              ];
-              user = "greeter";
-            };
+          default_session = {
+            command = lib.concatStringsSep " " [
+              (lib.getExe pkgs.greetd.tuigreet)
+              "--time"
+              "--remember"
+              "--remember-user-session"
+              "--asterisks"
+              "--sessions 'hyprland'"
+              "--cmd 'hyprland'"
+            ];
+            user = "greeter";
+          };
         };
       };
     };
