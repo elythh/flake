a61955c0170e43e70b7804b71e3a5b9983ed002a
elythh <gwenchlan@tuta.io>
2024-08-24 17:09:10 +0200

    :sparkles: (secrets): init sops

---
+++ modules/home/opt/utils/sops/default.nix
@@ -0,0 +1,17 @@
+{ config, inputs, ... }:
+{
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+
+  config = {
+    sops = {
+      defaultSopsFile = ../../../../../secrets/secrets.yaml;
+      defaultSopsFormat = "yaml";
+
+      age = {
+        generateKey = true;
+        keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+        sshKeyPaths = [ "${config.home.homeDirectory}/.ssh/id_ed25519" ];
+      };
+    };
+  };
+}
