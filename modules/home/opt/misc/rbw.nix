afebdf9b7efe727743ede2123133b7583216620e
elythh <gwenchlan@tuta.io>
2024-08-25 14:30:13 +0200

    :lock: (rbw): config as a secret

--- modules/home/opt/misc/rbw.nix
+++ modules/home/opt/misc/rbw.nix
@@ -1,15 +1,6 @@
 { config, lib, ... }:
 lib.mkIf config.modules.rbw.enable {
-  home.file.".config/rbw/config.json".text = ''
-    {
-      "email":"gwenchlan@tuta.io",
-      "base_url":null,
-      "identity_url":null,
-      "notifications_url":null,
-      "lock_timeout":99999999999999999,
-      "sync_interval":3600,
-      "pinentry":"pinentry",
-      "client_cert_path":null
-    }
-  '';
+  sops.secrets.rbw = {
+    path = "${config.home.homeDirectory}/.config/rbw/config.json";
+  };
 }
