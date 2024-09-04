a070c92a341953bfa41a3ef7748c07342272497a
elythh <gwenchlan@tuta.io>
2024-09-04 17:44:01 +0200

    :wrench: (lazygit): use nix syntaxe from home manager

--- modules/home/opt/utils/lazygit/default.nix
+++ modules/home/opt/utils/lazygit/default.nix
@@ -6,51 +6,63 @@
 }:
 {
   config = lib.mkIf config.modules.lazygit.enable {
-    programs.lazygit.enable = true;
     home.packages = with pkgs; [ difftastic ];
-
-    home.file.".config/lazygit/config.yml".text = ''
-      gui:
-        nerdFontsVersion: 3
-        showDivergenceFromBaseBranch: onlyArrow
-        filterMode: fuzzy
-        spinner:
-          # The frames of the spinner animation.
-          frames:
-            - "⠋"
-            - "⠙"
-            - "⠩"
-            - "⠸"
-            - "⠼"
-            - "⠴"
-            - "⠦"
-            - "⠧"
-          rate: 60
-      git:
-        parseEmoji: true
-        overrideGpg: true
-        paging:
-          externalDiffCommand: difft --color=always --display=inline --syntax-highlight=on
-
-      customCommands:
-        - key: "E"
-          command: "gitmoji commit"
-          description: "commit with gitmoji"
-          context: "files"
-          loadingText: "opening gitmoji commit tool"
-          subprocess: true
-        - key: "C"
-          command: "wanda git commit"
-          description: "commit with cz"
-          context: "files"
-          loadingText: "opening cz commit tool"
-          subprocess: true
-        - key: "c"
-          command: "git commit"
-          description: "commit"
-          context: "files"
-          loadingText: "opening vim"
-          subprocess: true
-    '';
+    programs.lazygit = {
+      enable = true;
+      settings = {
+        gui = {
+          nerdFontsVersion = 3;
+          showDivergenceFromBaseBranch = "onlyArrow";
+          filterMode = "fuzzy";
+          spinner = {
+            # The frames of the spinner animation.
+            frames = [
+              "⠋"
+              "⠙"
+              "⠩"
+              "⠸"
+              "⠼"
+              "⠴"
+              "⠦"
+              "⠧"
+            ];
+            rate = 60;
+          };
+        };
+        git = {
+          parseEmoji = true;
+          overrideGpg = true;
+          paging = {
+            externalDiffCommand = "difft --color=always --syntax-highlight=on";
+          };
+        };
+        customCommands = [
+          {
+            key = "E";
+            command = "gitmoji commit";
+            description = "commit with gitmoji";
+            context = "files";
+            loadingText = "opening gitmoji commit tool";
+            subprocess = true;
+          }
+          {
+            key = "C";
+            command = "wanda git commit";
+            description = "commit with cz";
+            context = "files";
+            loadingText = "opening cz commit tool";
+            subprocess = true;
+          }
+          {
+            key = "c";
+            command = "git commit";
+            description = "commit";
+            context = "files";
+            loadingText = "opening vim";
+            subprocess = true;
+          }
+        ];
+      };
+    };
   };
 }
