7e78837eb84bf42887b90ef6e21e06a4115c1444
elyth <gwen@omg.lol>
2024-09-19 16:48:24 +0200

    :wrench: (os): tpm option

--- hosts/aurelionite/default.nix
+++ hosts/aurelionite/default.nix
@@ -12,8 +12,5 @@
   fonts.enable = true;
   wayland.enable = true;
   pipewire.enable = true;
-  security.tpm2.enable = true;
-  security.tpm2.pkcs11.enable = true; # expose /run/current-system/sw/lib/libtpm2_pkcs11.so
-  security.tpm2.tctiEnvironment.enable = true; # TPM2TOOLS_TCTI and TPM2_PKCS11_TCTI env variables
-  users.users.gwen.extraGroups = [ "tss" ]; # tss group has access to TPM devices
+  tpm.enable = true;
 }
