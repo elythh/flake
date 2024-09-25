80fea3874b1f423e38a09b257553679889a81f33
elythh <gwenchlan@tuta.io>
2024-09-25 18:05:05 +0200

    :package: (flake): change zen source, use fufexan

--- flake.nix
+++ flake.nix
@@ -72,7 +72,7 @@
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
 
     # DELETEME: Zen
-    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    zen-browser.url = "github:fufexan/zen-browser-flake";
   };
 
   outputs =
