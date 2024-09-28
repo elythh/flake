16320480501d57d0f0ab56e1a7246b51c27a4de6
elythh <gwen@omg.lol>
2024-09-28 14:27:05 +0200

    :wrench: (immich): cloudflare tunnel config and secret

--- modules/nixos/opt/services/cloudflared-tunnel.nix
+++ modules/nixos/opt/services/cloudflared-tunnel.nix
@@ -13,17 +13,6 @@ in
   config = mkIf cfg.enable {
     services.cloudflared = {
       enable = true;
-      tunnels = {
-        "9f52eb17-9286-4b74-8526-28094e48f79f" = {
-          credentialsFile = config.sops.secrets.cloudflared-tunnel-creds.path;
-          default = "http_status:404";
-          ingress = {
-            "photo.coopteam.work" = {
-              service = "http://localhost:3333";
-            };
-          };
-        };
-      };
     };
     sops.secrets.cloudflared-tunnel-creds = {
       path = "/run/secret/cloudflare";
