c26dd6ca4f7991aea080236c4f21bac0eb73004b
elythh <gwenchlan@tuta.io>
2024-10-04 10:12:04 +0200

    :wrench: (nixvim): obsidian config belongs here

--- modules/home/opt/nvim/nixvim.nix
+++ modules/home/opt/nvim/nixvim.nix
@@ -32,7 +32,104 @@ let
         };
         enable = lib.mkForce true;
       };
-      plugins.obsidian.enable = lib.mkForce true;
+      plugins.obsidian = {
+        enable = lib.mkForce true;
+        workspaces = lib.mkForce [
+          {
+            name = "work";
+            path = "~/obsidian/work";
+          }
+          {
+            name = "home";
+            path = "~/obsidian/home";
+          }
+        ];
+        daily_notes = {
+          template = "~/obsidian/templates/daily_note_template.md";
+          folder = "~/obsidian/work/daily_notes";
+        };
+        templates = {
+          subdir = "~/obsidian/templates";
+          substitutions = {
+            monday.__raw = ''
+              function()
+                  -- Get the current date
+                  local currentDate = os.date("*t")
+
+                  -- Calculate the difference in days to the first day of the week (Monday)
+                  local diff = (currentDate.wday + 5) % 7
+
+                  -- Subtract the difference to get the date of the first day of the week
+                  currentDate.day = currentDate.day - diff
+
+                  -- Return the date of the first day of the week
+                  return os.date("%Y-%m-%d", os.time(currentDate))
+              end
+            '';
+            tuesday.__raw = ''
+              function()
+                  -- Get the current date
+                  local currentDate = os.date("*t")
+
+                  -- Calculate the difference in days to the first day of the week (Monday)
+                  local diff = (currentDate.wday + 4) % 7
+
+                  -- Subtract the difference to get the date of the first day of the week
+                  currentDate.day = currentDate.day - diff
+
+                  -- Return the date of the first day of the week
+                  return os.date("%Y-%m-%d", os.time(currentDate))
+              end
+            '';
+            wednesday.__raw = ''
+              function()
+                  -- Get the current date
+                  local currentDate = os.date("*t")
+
+                  -- Calculate the difference in days to the first day of the week (Monday)
+                  local diff = (currentDate.wday + 3) % 7
+
+                  -- Subtract the difference to get the date of the first day of the week
+                  currentDate.day = currentDate.day - diff
+
+                  -- Return the date of the first day of the week
+                  return os.date("%Y-%m-%d", os.time(currentDate))
+              end
+            '';
+            thursday.__raw = ''
+              function()
+                  -- Get the current date
+                  local currentDate = os.date("*t")
+
+                  -- Calculate the difference in days to the first day of the week (Monday)
+                  local diff = (currentDate.wday + 2) % 7
+
+                  -- Subtract the difference to get the date of the first day of the week
+                  currentDate.day = currentDate.day - diff
+
+                  -- Return the date of the first day of the week
+                  return os.date("%Y-%m-%d", os.time(currentDate))
+              end
+            '';
+            friday.__raw = ''
+              function()
+                  -- Get the current date
+                  local currentDate = os.date("*t")
+
+                  -- Calculate the difference in days to the first day of the week (Monday)
+                  local diff = (currentDate.wday + 1) % 7
+
+                  -- Subtract the difference to get the date of the first day of the week
+                  currentDate.day = currentDate.day - diff
+
+                  -- Return the date of the first day of the week
+                  return os.date("%Y-%m-%d", os.time(currentDate))
+              end
+            '';
+          };
+        };
+
+      };
       plugins.indent-blankline.enable = lib.mkForce false;
       assistant = "copilot";
     };
