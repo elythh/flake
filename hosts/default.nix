41eca91b3abe09f22dd352179379ddaa2dbb8c15
elyth <gwen@omg.lol>
2024-10-10 15:43:29 +0200

    :wrench: (flake): nixosSystem refact

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,28 +1,24 @@
 { self, inputs, ... }:
 {
-  flake = {
-    nixosConfigurations =
-      let
-        inherit (inputs.nixpkgs.lib) nixosSystem;
-        inherit (import "${self}/modules/nixos") default;
+  flake.nixosConfigurations =
+    let
+      inherit (inputs.nixpkgs.lib) nixosSystem;
+      inherit (import "${self}/modules/nixos") default;
 
-        specialArgs = {
-          inherit inputs self;
-        };
-      in
-      {
-        grovetender = nixosSystem {
-          inherit specialArgs;
-          modules = default ++ [ ./grovetender ];
-        };
-        aurelionite = nixosSystem {
-          inherit specialArgs;
-          modules = default ++ [ ./aurelionite ];
-        };
-        mithrix = nixosSystem {
-          inherit specialArgs;
-          modules = default ++ [ ./mithrix ];
-        };
+      specialArgs = {
+        inherit inputs self;
       };
-  };
+
+      mkHost =
+        hostname:
+        nixosSystem {
+          inherit specialArgs;
+          modules = default ++ [ ./${hostname} ];
+        };
+    in
+    {
+      grovetender = mkHost "grovetender";
+      aurelionite = mkHost "aurelionite";
+      mithrix = mkHost "mithrix";
+    };
 }
