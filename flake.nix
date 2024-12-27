0d0446f134636cfa1e09e1b5e3ef029f98c84b5c
elyth <gwen@omg.lol>
2024-12-27 23:55:45 +0100

    :sparkles: (ghosty): init
    hype i guess

--- flake.nix
+++ flake.nix
@@ -71,6 +71,9 @@
     # DELETEME: Zen
     zen-browser.url = "github:0xc000022070/zen-browser-flake";
 
+    # Ghostty, the overhyped terminal
+    ghostty.url = "github:ghostty-org/ghostty";
+
     sss.url = "github:elythh/sss/fix-sga";
     sss.inputs.nixpkgs.follows = "nixpkgs";
   };
