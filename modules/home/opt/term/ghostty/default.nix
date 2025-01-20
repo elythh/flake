36b6576d4ab14c7a71ef39a4dfbf574465b556d7
elyth <gwen@omg.lol>
2025-01-20 11:28:49 +0100

    :sparkles: (terminal): add ghostty

---
+++ modules/home/opt/term/ghostty/default.nix
@@ -0,0 +1,18 @@
+{ config, lib, ... }:
+lib.mkIf (config.default.terminal == "ghostty") {
+  home.sessionVariables.TERMINAL = "ghostty";
+  programs.ghostty = {
+    enable = true;
+    settings = {
+      window-decoration = false;
+      window-padding-x = "10,10";
+      window-padding-y = "10,10";
+      keybind = [
+        "alt+two=unbind"
+        "alt+one=unbind"
+        "alt+three=unbind"
+        "alt+four=unbind"
+      ];
+    };
+  };
+}
