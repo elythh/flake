a71f962562d27119312f32bc72c6135f9396bc60
elyth <gwen@omg.lol>
2025-01-23 11:42:09 +0100

    :art: (starship): random icon padding

--- modules/home/opt/shell/starship/default.nix
+++ modules/home/opt/shell/starship/default.nix
@@ -28,10 +28,10 @@
       character = {
         success_symbol = "[ ](#${base05} bold)";
         error_symbol = "[ ](#${base08} bold)";
-        vicmd_symbol = "[](#${base03})";
+        vicmd_symbol = "[ ](#${base03})";
       };
       directory = {
-        format = "[]($style)[  ](bg:#${base01} fg:#${base05})[$path](bg:#${base01} fg:#${base05} bold)[ ]($style)";
+        format = "[]($style)[ ](bg:#${base01} fg:#${base05})[$path](bg:#${base01} fg:#${base05} bold)[ ]($style)";
         style = "bg:none fg:#${base01}";
         truncation_length = 3;
         truncate_to_repo = false;
@@ -47,7 +47,7 @@
         ahead = "[⇡\${count} ](fg:#${base0B} bg:#${base01}) ";
         behind = "[⇣\${count} ](fg:#${base08} bg:#${base01})";
         diverged = "↑\${ahead_count} ⇣\${behind_count} ";
-        up_to_date = "[ ](fg:#${base0B} bg:#${base01})";
+        up_to_date = "[](fg:#${base0B} bg:#${base01})";
         untracked = "[?\${count} ](fg:#${base09} bg:#${base01}) ";
         stashed = "";
         modified = "[~\${count} ](fg:#${base09} bg:#${base01})";
@@ -58,7 +58,7 @@
       cmd_duration = {
         min_time = 1;
         # duration & style ;
-        format = "[]($style)[[  ](bg:#${base01} fg:#${base08} bold)$duration](bg:#${base01} fg:#${base05} bold)[]($style)";
+        format = "[]($style)[[ ](bg:#${base01} fg:#${base08} bold)$duration](bg:#${base01} fg:#${base05} bold)[]($style)";
         disabled = false;
         style = "bg:none fg:#${base01}";
       };
@@ -72,7 +72,7 @@
         unknown_msg = "";
       };
       kubernetes = {
-        format = "[](fg:#${base01} bg:none)[  ](fg:#${base0D} bg:#${base01})[$context/$namespace]($style)[](fg:#${base01} bg:none) ";
+        format = "[](fg:#${base01} bg:none)[ ](fg:#${base0D} bg:#${base01})[$context/$namespace]($style)[](fg:#${base01} bg:none) ";
         disabled = false;
         style = "fg:#${base05} bg:#${base01} bold";
         context_aliases = {
@@ -89,7 +89,7 @@
         disabled = false;
       };
       azure = {
-        format = "[](fg:#${base01} bg:none)[󰠅  ](fg:#${base0E} bg:#${base01})[$subscription]($style)[](fg:#${base01} bg:none) ";
+        format = "[](fg:#${base01} bg:none)[󰠅 ](fg:#${base0E} bg:#${base01})[$subscription]($style)[](fg:#${base01} bg:none) ";
         style = "fg:#${base05} bg:#${base01} bold";
         disabled = false;
       };
