f94ba869e9690f5a1f06bcefc0461d61d9230605
elyth <gwen@omg.lol>
2025-01-27 18:56:30 +0100

    :art: (font): caskaydia

--- modules/home/core/style/stylix.nix
+++ modules/home/core/style/stylix.nix
@@ -35,8 +35,8 @@
     fonts = {
       sizes.terminal = 13;
       monospace = {
-        name = "GeistMono Nerd Font";
-        package = pkgs.nerd-fonts.geist-mono;
+        name = "CaskaydiaMono Nerd Font";
+        package = pkgs.nerd-fonts.caskaydia-mono;
       };
       sansSerif = {
         name = "IBM Plex Sans";
