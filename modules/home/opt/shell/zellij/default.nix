1f7482afe28a15dc55119408d470e414fe5f325f
elyth <gwen@omg.lol>
2025-01-27 18:56:53 +0100

    :wrench: (zellij): layout moved to simple and plugin def

--- modules/home/opt/shell/zellij/default.nix
+++ modules/home/opt/shell/zellij/default.nix
@@ -53,7 +53,7 @@ in
       enableZshIntegration = false;
     };
 
-    xdg.configFile."zellij/config.kdl".text = ''
+    xdg.configFile."zellij/config.kdl".text = with config.lib.stylix.colors; ''
       default_layout "default"
       mouse_mode true
       copy_on_select true
@@ -88,7 +88,7 @@ in
                           SwitchToMode "Normal";
                       }
                       bind "Ctrl f" {
-                              LaunchOrFocusPlugin "file:~/.config/zellij/plugins/monocle.wasm" {
+                              LaunchOrFocusPlugin "https://github.com/imsnif/monocle/releases/download/v0.100.2/monocle.wasm" {
                                       floating true
                               }
                               SwitchToMode "Normal"
@@ -101,104 +101,9 @@ in
 
             unbind "Ctrl b" "Ctrl q" 
           }
-    '';
 
-    xdg.configFile."zellij/layouts/default.kdl".text = with config.lib.stylix.colors; ''
-      layout {
-          swap_tiled_layout name="vertical" {
-              tab max_panes=5 {
-                  pane split_direction="vertical" {
-                      pane
-                      pane { children; }
-                  }
-              }
-              tab max_panes=8 {
-                  pane split_direction="vertical" {
-                      pane { children; }
-                      pane { pane; pane; pane; pane; }
-                  }
-              }
-              tab max_panes=12 {
-                  pane split_direction="vertical" {
-                      pane { children; }
-                      pane { pane; pane; pane; pane; }
-                      pane { pane; pane; pane; pane; }
-                  }
-              }
-          }
-
-          swap_tiled_layout name="horizontal" {
-              tab max_panes=5 {
-                  pane
-                  pane
-              }
-              tab max_panes=8 {
-                  pane {
-                      pane split_direction="vertical" { children; }
-                      pane split_direction="vertical" { pane; pane; pane; pane; }
-                  }
-              }
-              tab max_panes=12 {
-                  pane {
-                      pane split_direction="vertical" { children; }
-                      pane split_direction="vertical" { pane; pane; pane; pane; }
-                      pane split_direction="vertical" { pane; pane; pane; pane; }
-                  }
-              }
-          }
-
-          swap_tiled_layout name="stacked" {
-              tab min_panes=5 {
-                  pane split_direction="vertical" {
-                      pane
-                      pane stacked=true { children; }
-                  }
-              }
-          }
-
-          swap_floating_layout name="staggered" {
-              floating_panes
-          }
-
-          swap_floating_layout name="enlarged" {
-              floating_panes max_panes=10 {
-                  pane { x "5%"; y 1; width "90%"; height "90%"; }
-                  pane { x "5%"; y 2; width "90%"; height "90%"; }
-                  pane { x "5%"; y 3; width "90%"; height "90%"; }
-                  pane { x "5%"; y 4; width "90%"; height "90%"; }
-                  pane { x "5%"; y 5; width "90%"; height "90%"; }
-                  pane { x "5%"; y 6; width "90%"; height "90%"; }
-                  pane { x "5%"; y 7; width "90%"; height "90%"; }
-                  pane { x "5%"; y 8; width "90%"; height "90%"; }
-                  pane { x "5%"; y 9; width "90%"; height "90%"; }
-                  pane focus=true { x 10; y 10; width "90%"; height "90%"; }
-              }
-          }
-
-          swap_floating_layout name="spread" {
-              floating_panes max_panes=1 {
-                  pane {y "50%"; x "50%"; }
-              }
-              floating_panes max_panes=2 {
-                  pane { x "1%"; y "25%"; width "45%"; }
-                  pane { x "50%"; y "25%"; width "45%"; }
-              }
-              floating_panes max_panes=3 {
-                  pane focus=true { y "55%"; width "45%"; height "45%"; }
-                  pane { x "1%"; y "1%"; width "45%"; }
-                  pane { x "50%"; y "1%"; width "45%"; }
-              }
-              floating_panes max_panes=4 {
-                  pane { x "1%"; y "55%"; width "45%"; height "45%"; }
-                  pane focus=true { x "50%"; y "55%"; width "45%"; height "45%"; }
-                  pane { x "1%"; y "1%"; width "45%"; height "45%"; }
-                  pane { x "50%"; y "1%"; width "45%"; height "45%"; }
-              }
-          }
-
-          default_tab_template {
-              pane size=2 borderless=true {
-                  plugin location="https://github.com/dj95/zjstatus/releases/download/v0.19.0/zjstatus.wasm" {
+      plugins {
+              zjstatus location="https://github.com/dj95/zjstatus/releases/download/v0.20.0/zjstatus.wasm" {
                       format_left   "{mode}#[bg=#181926] {tabs}"
                       format_center ""
                       format_right  "#[bg=#${base0D},fg=#${base01},bold]  #[bg=#${base01},fg=#${base0D},bold] {session} #[bg=#${base01},fg=#${base0D},bold]"
@@ -253,9 +158,17 @@ in
                       datetime_format "%A, %d %b %Y %H:%M"
                       datetime_timezone "Europe/London"
                   }
-              }
-              children
-          }
+      }
+    '';
+
+    xdg.configFile."zellij/layouts/default.kdl".text = ''
+      layout {
+        default_tab_template {
+            children
+            pane size=1 borderless=true {
+                plugin location="zjstatus"
+            }
+        }
       }
     '';
 
