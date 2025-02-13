a29e9988dfbcfae4c0038ce19ff6b7fbf9809f24
elyth <gwen@omg.lol>
2025-02-13 02:12:57 +0100

    :package: (flake): switch to new nixvim config

--- modules/home/opt/nvim/nixvim.nix
+++ modules/home/opt/nvim/nixvim.nix
@@ -1,49 +1,8 @@
 {
   inputs,
-  config,
-  lib,
+  pkgs,
   ...
 }:
-let
-  enable-avante-module = {
-    plugins.avante = {
-      enable = lib.mkForce true;
-    };
-  };
-  set-custom-theme-module = {
-    theme = lib.mkForce "${config.theme}";
-    colorschemes.base16 = {
-      colorscheme = lib.mkForce {
-        inherit (config.lib.stylix.colors.withHashtag)
-          base00
-          base01
-          base02
-          base03
-          base04
-          base05
-          base06
-          base07
-          base08
-          base09
-          base0A
-          base0B
-          base0C
-          base0D
-          base0E
-          base0F
-          ;
-      };
-    };
-  };
-  inherit (inputs.nixvim.nixvimConfigurations."x86_64-linux") nixvim;
-  nixvimExtended = nixvim.extendModules {
-    modules = [
-      enable-avante-module
-      set-custom-theme-module
-    ];
-  };
-  elythvim = nixvimExtended.config.build.package;
-in
 {
-  home.packages = [ elythvim ];
+  home.packages = [ inputs.nixvim.packages.${pkgs.system}.default ];
 }
