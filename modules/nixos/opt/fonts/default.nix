12029b8215f047c1fbb2e149b742bfc26db120aa
elythh <gwenchlan@tuta.io>
2025-02-19 13:36:06 +0100

    :fire: (fonts): I dont need you

--- modules/nixos/opt/fonts/default.nix
+++ modules/nixos/opt/fonts/default.nix
@@ -9,19 +9,6 @@
     fonts.packages = with pkgs; [
       # icon fonts
       material-design-icons
-
-      # normal fonts
-      rubik
-      lexend
-      noto-fonts
-      roboto
-      google-fonts
-
-      nerd-fonts.fira-code
-      nerd-fonts.fantasque-sans-mono
-      nerd-fonts.zed-mono
-      nerd-fonts.iosevka
-      nerd-fonts.monaspace
     ];
   };
 }
