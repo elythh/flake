8ecd244147db7f6d966ea1facc1ca88d723f736b
elythh <gwenchlan@tuta.io>
2025-02-19 13:53:46 +0100

    :wrench: (yamllint): add config

---
+++ modules/home/opt/misc/yamllint.nix
@@ -0,0 +1,51 @@
+{ config, lib, ... }:
+let
+  inherit (lib)
+    mkIf
+    mkEnableOption
+    ;
+
+  cfg = config.opt.misc.yamllint;
+in
+{
+  options.opt.misc.yamllint = {
+    enable = mkEnableOption "Wether to enable yamllint";
+  };
+  config = mkIf cfg.enable {
+    home.file.".config/yamllint/config".text = ''
+      yaml-files:
+        - '*.yaml'
+        - '*.yml'
+        - '.yamllint'
+
+      rules:
+        anchors: enable
+        braces: enable
+        brackets: enable
+        colons: enable
+        commas: enable
+        comments:
+          level: warning
+        comments-indentation:
+          level: warning
+        document-end: disable
+        document-start:
+          level: warning
+        empty-lines: enable
+        empty-values: disable
+        float-values: disable
+        hyphens: enable
+        indentation: enable
+        key-duplicates: enable
+        key-ordering: disable
+        line-length: disable
+        new-line-at-end-of-file: enable
+        new-lines: enable
+        octal-values: disable
+        quoted-strings: disable
+        trailing-spaces: enable
+        truthy:
+          level: warning
+    '';
+  };
+}
