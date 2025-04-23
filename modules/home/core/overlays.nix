150fc914cc42ecc50fafad8a659588b7cc8d57a5
elythh <gwenchlan@tuta.io>
2025-04-23 19:07:09 +0200

    :bug: broken upstream

--- modules/home/core/overlays.nix
+++ modules/home/core/overlays.nix
@@ -1,16 +1,22 @@
 { inputs, ... }:
 {
   nixpkgs.overlays = [
+    (final: prev: {
+      qt6Packages = prev.qt6Packages.overrideScope (
+        _: kprev: {
+          qt6gtk2 = kprev.qt6gtk2.overrideAttrs (_: {
+            version = "0.5-unstable-2025-03-04";
+            src = final.fetchFromGitLab {
+              domain = "opencode.net";
+              owner = "trialuser";
+              repo = "qt6gtk2";
+              rev = "d7c14bec2c7a3d2a37cde60ec059fc0ed4efee67";
+              hash = "sha256-6xD0lBiGWC3PXFyM2JW16/sDwicw4kWSCnjnNwUT4PI=";
+            };
+          });
+        }
+      );
+    })
     inputs.nur.overlays.default
-    # (_: prev: {
-    #   # Fix slack screen sharing following: https://github.com/flathub/com.slack.Slack/issues/101#issuecomment-1807073763
-    #   slack = prev.slack.overrideAttrs (previousAttrs: {
-    #     installPhase =
-    #       previousAttrs.installPhase
-    #       + ''
-    #         sed -i'.backup' -e 's/,"WebRTCPipeWireCapturer"/,"LebRTCPipeWireCapturer"/' $out/lib/slack/resources/app.asar
-    #       '';
-    #   });
-    # })
   ];
 }
