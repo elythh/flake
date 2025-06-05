081c4af43ae52fbf8cdb339c5b2ee61fa1995cb9
elyth <gwen@omg.lol>
2025-06-05 22:07:01 +0200

    :wrench: (nixpkgs): already declared

--- hosts/grovetender/hardware.nix
+++ hosts/grovetender/hardware.nix
@@ -126,6 +126,5 @@
   # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
