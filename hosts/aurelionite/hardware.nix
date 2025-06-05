179b3028c93ee07a7ae25be132e424f16ccf5e06
elyth <gwen@omg.lol>
2025-06-05 22:07:01 +0200

    :wrench: (nixpkgs): already declared

--- hosts/aurelionite/hardware.nix
+++ hosts/aurelionite/hardware.nix
@@ -43,6 +43,5 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp108s0.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
