54a3a0a67826e5e56b69628ad1c4003ccd708dd3
SARWAR <sarrwar16@gmail.com>
2025-06-12 16:34:56 +0600

    encrypt

--- flake.nix
+++ flake.nix
@@ -35,6 +35,7 @@
               ];
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+              home-manager.backupFileExtension = "backup";
               home-manager.extraSpecialArgs = {
                 inherit inputs outputs lib;
               };
