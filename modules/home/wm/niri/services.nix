2eeed0c0dd05dddd366813e027a9c674b7399a3f
elyth <gwen@omg.lol>
2025-08-25 14:17:23 +0200

    :bug: (niri): make svc optionals

--- modules/home/wm/niri/services.nix
+++ modules/home/wm/niri/services.nix
@@ -1,63 +1,71 @@
 {
   pkgs,
+  lib,
+  config,
   ...
 }:
+let
+  inherit (lib) mkIf;
+  cfg = config.meadow.default.wm == "niri";
+in
 {
-  home.packages = with pkgs; [
-    libnotify
-    polkit_gnome
-    squeekboard
-  ];
+  config = mkIf cfg {
+    home.packages = with pkgs; [
+      libnotify
+      polkit_gnome
+      squeekboard
+    ];
 
-  # https://github.com/emersion/mako/blob/master/doc/mako.5.scd
-  services.mako = {
-    enable = true;
-    settings = {
-      actions = true;
-      markup = true;
-      icons = true;
-      layer = "overlay";
-      anchor = "top-right";
-      border-size = 0;
-      border-radius = 10;
-      padding = 10;
-      width = 330;
-      height = 200;
-      default-timeout = 5000;
-      max-icon-size = 32;
-    };
-  };
-
-  systemd.user.services = {
-    squeekboard = {
-      Unit = {
-        Description = "On-Screen Keyboard";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
-      };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.squeekboard}/bin/.squeekboard-wrapped";
-        Restart = "on-failure";
-      };
-      Install = {
-        WantedBy = [ "niri.service" ];
+    # https://github.com/emersion/mako/blob/master/doc/mako.5.scd
+    services.mako = {
+      enable = true;
+      settings = {
+        actions = true;
+        markup = true;
+        icons = true;
+        layer = "overlay";
+        anchor = "top-right";
+        border-size = 0;
+        border-radius = 10;
+        padding = 10;
+        width = 330;
+        height = 200;
+        default-timeout = 5000;
+        max-icon-size = 32;
       };
     };
 
-    polkit-gnome-authentication-agent-1 = {
-      Unit = {
-        Description = "GNOME Polkit Authentication Agent";
-        After = [ "graphical-session.target" ];
-        PartOf = [ "graphical-session.target" ];
+    systemd.user.services = {
+      squeekboard = {
+        Unit = {
+          Description = "On-Screen Keyboard";
+          After = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.squeekboard}/bin/.squeekboard-wrapped";
+          Restart = "on-failure";
+        };
+        Install = {
+          WantedBy = [ "niri.service" ];
+        };
       };
-      Service = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-      };
-      Install = {
-        WantedBy = [ "niri.service" ];
+
+      polkit-gnome-authentication-agent-1 = {
+        Unit = {
+          Description = "GNOME Polkit Authentication Agent";
+          After = [ "graphical-session.target" ];
+          PartOf = [ "graphical-session.target" ];
+        };
+        Service = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+        };
+        Install = {
+          WantedBy = [ "niri.service" ];
+        };
       };
     };
   };
