23a9d5647a01df61fbde553653ca1257212e0d93
elyth <gwen@omg.lol>
2026-02-09 17:23:31 +0100

    :wrench: (hyprland): auto start dank materialshell and kanshi

--- modules/home/wm/hyprland/config/options.nix
+++ modules/home/wm/hyprland/config/options.nix
@@ -3,13 +3,14 @@
     exec-once = [
       "clipse -listen"
       "wl-paste --type text --watch cliphist store"
-      "wl-paste --type text --watch cliphist store"
       "wl-paste --type image --watch cliphist store"
       "xprop -root -f _XWAYLAND_GLOBAL_OUTPUT_SCALE 32c -set _XWAYLAND_GLOBAL_OUTPUT_SCALE 1"
       "dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
       "systemctl --user import-environment PATH"
       "systemctl --user restart xdg-desktop-portal.service"
       "systemctl --user restart xdg-desktop-portal-wlr.service"
+      "systemctl --user start kanshi.service"
+      "dms run"
     ];
     animations = {
       enabled = true;
