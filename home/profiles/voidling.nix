52ffd45fee563e27e501b3d2abdb0ff37d06fd86
elyth <gwen@omg.lol>
2026-05-08 19:31:08 +0200

    📦️ Openldap is broken

--- home/profiles/voidling.nix
+++ home/profiles/voidling.nix
@@ -46,7 +46,29 @@
       stremio-linux-shell
       easyeffects
       r2modman
-      # lutris
+      (pkgs.lutris.override {
+        # Intercept buildFHSEnv to modify target packages
+        buildFHSEnv =
+          args:
+          pkgs.buildFHSEnv (
+            args
+            // {
+              multiPkgs =
+                envPkgs:
+                let
+                  # Fetch original package list
+                  originalPkgs = args.multiPkgs envPkgs;
+
+                  # Disable tests for openldap
+                  customLdap = envPkgs.openldap.overrideAttrs (_: {
+                    doCheck = false;
+                  });
+                in
+                # Replace broken openldap with the custom one
+                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+            }
+          );
+      })
       zoom-us
       mangohud
       deadlock-mod-manager
