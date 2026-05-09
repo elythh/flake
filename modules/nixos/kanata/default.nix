315fe6ebf3cc491082e85d1b3591c66355a2dc20
elyth <gwen@omg.lol>
2026-05-09 20:16:03 +0200

    :wrench: Homerow mods

--- modules/nixos/kanata/default.nix
+++ modules/nixos/kanata/default.nix
@@ -20,17 +20,29 @@ in
         config = ''
           (defsrc
            caps
+           a s d f
+           j k l ;
           )
           (defvar
-           tap-time 150
+           tap-time 200
            hold-time 200
           )
           (defalias
-           caps (tap-hold 100 100 esc lctl)
+            caps (tap-hold 100 100 esc lctl)
+            hm_a (tap-hold $tap-time $hold-time a lmet)
+            hm_s (tap-hold $tap-time $hold-time s lalt)
+            hm_d (tap-hold $tap-time $hold-time d lctl)
+            hm_f (tap-hold $tap-time $hold-time f lsft)
+            hm_j (tap-hold $tap-time $hold-time j rsft)
+            hm_k (tap-hold $tap-time $hold-time k rctl)
+            hm_l (tap-hold $tap-time $hold-time l ralt)
+            hm_semicolon (tap-hold $tap-time $hold-time ; rmet)
           )
 
           (deflayer base
-           @caps
+            @caps
+            @hm_a @hm_s @hm_d @hm_f
+            @hm_j @hm_k @hm_l @hm_semicolon
           )
         '';
       };
