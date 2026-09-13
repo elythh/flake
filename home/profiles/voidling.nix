d2bcd8ce2d6400ba9dd1bb358153defb8e67cfb8
elyth <gwen@omg.lol>
2026-09-13 15:15:44 +0200

    🔥 Remove useless package overide

--- home/profiles/voidling.nix
+++ home/profiles/voidling.nix
@@ -53,29 +53,7 @@
       stremio-linux-shell
       easyeffects
       r2modman
-      (pkgs.lutris.override {
-        # Intercept buildFHSEnv to modify target packages
-        buildFHSEnv =
-          args:
-          pkgs.buildFHSEnv (
-            args
-            // {
-              multiPkgs =
-                envPkgs:
-                let
-                  # Fetch original package list
-                  originalPkgs = args.multiPkgs envPkgs;
-
-                  # Disable tests for openldap
-                  customLdap = envPkgs.openldap.overrideAttrs (_: {
-                    doCheck = false;
-                  });
-                in
-                # Replace broken openldap with the custom one
-                builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
-            }
-          );
-      })
+      lutris
       zoom-us
       mangohud
       deadlock-mod-manager
@@ -88,7 +66,6 @@
       thunar
       zenity
       inputs.fastpotify.packages.${pkgs.stdenv.hostPlatform.system}.default
-      # rustdesk
     ];
   };
 }
